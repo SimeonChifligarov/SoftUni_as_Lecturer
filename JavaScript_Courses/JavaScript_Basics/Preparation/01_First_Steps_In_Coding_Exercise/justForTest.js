62419bcd362523415c8bcb2259276342d4ecab18
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-17 19:53:29 +0300

    adding justForTest.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/01_First_Steps_In_Coding_Exercise/justForTest.js
@@ -0,0 +1,12 @@
+// function usdToBgn(input) {
+//     let usd = Number(input[0]);
+//     let bgn = usd * 180 / Math.PI;
+
+//     console.log(bgn);
+// }
+
+// usdToBgn(["3.1416"]);
+
+console.log(2.50 * 100);
+console.log(2.49 * 100);
+console.log(0.1 + 0.2);
