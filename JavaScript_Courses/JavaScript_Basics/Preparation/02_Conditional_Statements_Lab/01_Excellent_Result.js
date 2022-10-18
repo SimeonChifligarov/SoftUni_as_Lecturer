9cfd99476c59bb9bf50989a525ce674208d3859f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-18 19:40:00 +0300

    adding 01_Excellent_Result.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/02_Conditional_Statements_Lab/01_Excellent_Result.js
@@ -0,0 +1,17 @@
+function excellentResult(input) {
+    let result = Number(input[0]);
+    let excellentMessage = 'Excellent!'
+
+    if (result >= 5.50) {
+        console.log(excellentMessage);
+    }
+}
+
+// excellentResult(["6"]);
+// console.log('---');
+// excellentResult(["5"]);
+// console.log('---');
+// excellentResult(["5.50"]);
+// console.log('---');
+// excellentResult(["5.49"]);
+// console.log('---');
