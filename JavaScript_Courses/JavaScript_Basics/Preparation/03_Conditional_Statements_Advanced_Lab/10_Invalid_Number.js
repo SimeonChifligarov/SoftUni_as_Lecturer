0b99a754ae66f2c8c7fec869332e879f0eeab5a3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-20 18:41:34 +0300

    adding 10_Invalid_Number.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/03_Conditional_Statements_Advanced_Lab/10_Invalid_Number.js
@@ -0,0 +1,9 @@
+function invalidNumber(input) {
+    let number = Number(input[0]);
+
+    if (!((number >= 100 && number <= 200) || number == 0)) {
+        console.log("invalid");
+    }
+}
+
+// invalidNumber(["75"]);
