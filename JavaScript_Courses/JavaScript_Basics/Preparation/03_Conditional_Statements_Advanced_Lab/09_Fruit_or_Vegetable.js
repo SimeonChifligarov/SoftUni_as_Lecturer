5afda99e7d83f39ec8973c920b8354430d03fcd4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-20 18:41:07 +0300

    adding 09_Fruit_or_Vegetable.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/03_Conditional_Statements_Advanced_Lab/09_Fruit_or_Vegetable.js
@@ -0,0 +1,14 @@
+function fruitOrVegetable(input) {
+    let product = input[0];
+
+    if (product == "banana" || product == "apple" || product == "kiwi"
+        || product == "cherry" || product == "lemon" || product == "grapes") {
+        console.log("fruit");
+    } else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot") {
+        console.log("vegetable");
+    } else {
+        console.log("unknown");
+    }
+}
+
+// fruitOrVegetable(["banana"]);
