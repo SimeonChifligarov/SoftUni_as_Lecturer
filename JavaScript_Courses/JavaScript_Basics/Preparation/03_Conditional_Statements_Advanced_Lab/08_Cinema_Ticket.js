1776493ccbfafe755446723c4c9051309ec33c12
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-20 18:40:42 +0300

    adding 08_Cinema_Ticket.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/03_Conditional_Statements_Advanced_Lab/08_Cinema_Ticket.js
@@ -0,0 +1,13 @@
+function cinemaTicket(input) {
+    let day = input[0];
+
+    if (day == "Monday" || day == "Tuesday" || day == "Friday") {
+        console.log(12);
+    } else if (day == "Wednesday" || day == "Thursday") {
+        console.log(14);
+    } else if (day == "Saturday" || day == "Sunday") {
+        console.log(16);
+    }
+}
+
+// cinemaTicket(["Monday"]);
