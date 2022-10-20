51fe2d86d883e487fe5482345583b862d58bc1af
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-20 18:39:09 +0300

    adding 04_Personal_Titles.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/03_Conditional_Statements_Advanced_Lab/04_Personal_Titles.js
@@ -0,0 +1,20 @@
+function personalTitles(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if (gender == "m") {
+        if (age >= 16) {
+            console.log("Mr.");
+        } else {
+            console.log("Master");
+        }
+    } else if (gender == "f") {
+        if (age >= 16) {
+            console.log("Ms.");
+        } else {
+            console.log("Miss");
+        }
+    }
+}
+
+// personalTitles(["17", "m"]);
