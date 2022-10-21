d648ec4ca8ca5463fd832e2ea0f025e0cad9a8dc
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-21 20:12:50 +0300

    adding 04_Fishing_Boat.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/03_Conditional_Statements_Advanced_Exercise/04_Fishing_Boat.js
@@ -0,0 +1,37 @@
+function fishingBoat(input) {
+    let budget = Number(input[0]);
+    let season = input[1];
+    let fishermen = Number(input[2]);
+
+    let totalCost = 0;
+
+    if (season == "Spring") {
+        totalCost = 3000;
+    } else if (season == "Summer") {
+        totalCost = 4200;
+    } else if (season == "Autumn") {
+        totalCost = 4200;
+    } else if (season == "Winter") {
+        totalCost = 2600;
+    }
+
+    if (fishermen <= 6) {
+        totalCost *= 0.90;
+    } else if (fishermen <= 11) {
+        totalCost *= 0.85;
+    } else {
+        totalCost *= 0.75;
+    }
+
+    if (season != "Autumn" && fishermen % 2 == 0) {
+        totalCost *= 0.95;
+    }
+
+    if (budget >= totalCost) {
+        console.log(`Yes! You have ${(budget - totalCost).toFixed(2)} leva left.`);
+    } else {
+        console.log(`Not enough money! You need ${(totalCost - budget).toFixed(2)} leva.`);
+    }
+}
+
+// fishingBoat(["3000", "Summer", "11"]);
