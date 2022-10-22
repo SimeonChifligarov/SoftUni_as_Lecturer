6e967b11429e85a7b9a89b760b0a3073872bbe1d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-22 09:45:24 +0300

    adding 93_Numbers_1_N_with_Step_3.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/04_For_Loop_Lab/03_Numbers_1_N_with_Step_3.js
@@ -0,0 +1,9 @@
+function numbers1ToNWithStep3(input) {
+    let number = Number(input[0]);
+
+    for (let i = 1; i <= number; i += 3) {
+        console.log(i);
+    }
+}
+
+// numbers1ToNWithStep3(["15"]);
