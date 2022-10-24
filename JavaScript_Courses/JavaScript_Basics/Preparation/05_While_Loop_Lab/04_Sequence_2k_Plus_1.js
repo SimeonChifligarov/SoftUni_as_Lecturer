5c4a6745ad5ee52465f26dbeea2b9ef02885339f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-24 19:49:41 +0300

    adding 04_Sequence_2k_Plus_1.js

---
+++ JavaScript_Courses/JavaScript_Basics/Preparation/05_While_Loop_Lab/04_Sequence_2k_Plus_1.js
@@ -0,0 +1,12 @@
+function sequence(input) {
+    let number = Number(input[0]);
+    let k = 1;
+    while (k <= number) {
+       console.log(k);
+       k = k * 2 + 1; 
+    }
+ }
+
+ 
+//  sequence(["31"]);
+ 
