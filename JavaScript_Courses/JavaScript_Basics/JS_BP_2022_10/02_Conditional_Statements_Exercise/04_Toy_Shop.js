1512171df567cac9fb27fe7319a6cdbc4a5bf619
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-29 10:56:47 +0300

    adding 04_Toy_Shop.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2022_10/02_Conditional_Statements_Exercise/04_Toy_Shop.js
@@ -0,0 +1,39 @@
+function toyShop(input) {
+    let puzzleCost = 2.60;
+    let dollCost = 3;
+    let teddyBearCost = 4.10;
+    let minionCost = 8.20;
+    let carCost = 2;
+
+    let vacationCost = Number(input[0]);
+    let puzzleCount = Number(input[1]);
+    let dollCount = Number(input[2]);
+    let teddyBearsCount = Number(input[3]);
+    let minionsCount = Number(input[4]);
+    let carsCount = Number(input[5]);
+
+    let totalCount = puzzleCount + dollCount + teddyBearsCount + minionsCount + carsCount;
+
+    let totalIncome = puzzleCost * puzzleCount + dollCost * dollCount
+        + teddyBearCost * teddyBearsCount + minionCost * minionsCount + carCost * carsCount;
+
+    if (totalCount >= 50) {
+        totalIncome *= 0.75;
+    }
+
+    totalIncome *= 0.90;  // after the НАЕМ;
+
+    // let difference = totalIncome - vacationCost;
+    // if (difference >= 0) {
+
+    // }
+
+    if (totalIncome >= vacationCost) {
+        console.log(`Yes! ${(totalIncome - vacationCost).toFixed(2)} lv left.`);
+    } else {
+        console.log(`Not enough money! ${(vacationCost - totalIncome).toFixed(2)} lv needed.`);
+    }
+
+}
+
+// toyShop(["40.8", "20", "25", "30", "50", "10"]);
