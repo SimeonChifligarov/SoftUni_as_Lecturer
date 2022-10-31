4fb38fe1fbbe0388f5a68c693f3a20c7cd7ffae3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-31 17:25:29 +0200

    adding 01_Numbers_Ending_in_7.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2022_10/04_For_Loop_Exercise/01_Numbers_Ending_in_7.js
@@ -0,0 +1,21 @@
+function numbersEndingIn7() {
+    // която отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+    for (let i = 1; i <= 1000; i++) {
+        // console.log(i);
+        if (i % 10 == 7) {
+            console.log(i);
+        }
+    }
+}
+
+// function numbersEndingIn7() {
+//     // която отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+//     for (let i = 7; i <= 997; i++) {
+//         // console.log(i);
+//         if (i % 10 == 7) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// numbersEndingIn7();
