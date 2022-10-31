8129e92b4f0dcc4b48cadbf94676b9274c9b02e4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-10-31 17:28:54 +0200

    adding justForTest.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2022_10/04_For_Loop_Exercise/justForTest.js
@@ -0,0 +1,66 @@
+// let a = 1337;
+
+// let b = a % 10;  // a / 10 => остатъка ; 133.7; 133; със остатък 7
+// let c = a % 100;  // а / 100 = 13; с остатък 37;
+
+// // console.log(b);
+// // console.log(c);
+
+
+// let t1 = 0.0000000000000000003;
+// let t2 = 0.0000000000000003;
+
+// t2 == t1 * 1000;
+
+
+// for (let i = 0; i < 100; i++) {
+//     console.log('OK!!!');
+// }
+
+
+// let a = 33;
+// let b = " е супер";
+// let c = "44";
+// let d = Number(c);  // explicit
+
+// console.log(a + c);  // 3344 string result
+// console.log(a - c);  // -11 result
+
+// let a = 5;
+
+// for (let i = 0; i < 100; i++) {  // i++; i--; i -= 1;
+//     console.log(i);
+// }
+
+// let a = 12;
+// a = 'text';
+
+// function salary(input) {
+//     for (let i = 0; i < input.length; i++) {
+//         let currentElement = input[i];
+//         console.log(currentElement);
+//     }
+
+//     console.log(currentElement);
+
+// }
+
+
+// salary([
+//     "10",
+//     "750",
+//     "Facebook",
+// ]);
+
+// let a = 'az';
+
+// for (let i = 0; i < 100; i++) {
+//     a += '1';
+//     console.log(a);
+//     if (a == 'az1111') {
+//         break;
+//     }
+// }
+
+
+console.log(-223 % 2);
