9741e853e98879fda83447782afaea299c00086e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-11-01 18:56:21 +0200

    adding justForTest.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2022_10/05_While_Loop_Exercise/justForTest.js
@@ -0,0 +1,50 @@
+
+// // let helper = 0;   // let command = input[i]; i++;
+// // while (cond1) {  // (command != "Stop")
+
+
+// //    cond1 = ...    // command = input[i]; i++;
+// // }
+
+// // break; 
+// // continue;
+
+// let num = 0;
+// while (true) {   // command != "Stop" ; money < 1000;
+//     if (cond1) {
+//         ...;
+//         break;
+//     }
+
+//     if (cond2) {
+//         ...;
+//         continue;
+//     }
+
+//     mainLogic...
+
+// }
+
+// // console.log(num);
+
+// let isFoundBook = true;  // true Или false
+// let isFoundBook = false;  // true Или false
+
+
+// if (isFoundBook) {  // logical NOT
+//     console.log('true!!');
+// } else {
+//     console.log('false!!!');
+// }
+
+
+while (a) {  // a && b && c
+    if (b) {
+        break;
+    }
+
+    if (c) {
+        break;
+    }
+
+}
