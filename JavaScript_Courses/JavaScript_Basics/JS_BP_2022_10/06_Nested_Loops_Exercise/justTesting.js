6e517f3f6da6157b82b9248c9cc07fcad05e8840
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-11-06 17:40:10 +0200

    adding justTesting.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2022_10/06_Nested_Loops_Exercise/justTesting.js
@@ -0,0 +1,26 @@
+
+// for (let i = 1; i <= 3; i++) {
+//     console.log(`i is: ${i}`);
+//     console.log('---');
+
+//     for (let j = 2; j <= 6; j++) {
+//         console.log(`*** j is ${j}`);
+//     }
+// }
+
+// let element = input[i]
+// i++
+
+
+// let a = 102448;
+
+// // a % 10  // 8, 4, 4, 2, 0, 1
+// // a = Math.round(a / 10)  // 10244, 1024, 102
+
+// let b = String(a)  // '102448';         [1, 2, 3, 4]   input[0] -> 1;    '1024' -> input[0] -> 1; input[1]
+
+// let c = 12;
+
+// c = c + 3;  // c += 3;
+
+// c++;     // c += 1; c = c + 1;
