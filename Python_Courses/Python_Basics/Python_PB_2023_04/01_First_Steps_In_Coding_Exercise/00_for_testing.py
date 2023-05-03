b61d08e20a457c970c1886d278ea458204e8eaac
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-03 19:45:27 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/01_First_Steps_In_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,85 @@
+# print("It works!")
+#
+# user = "Ivan"
+# print(user)
+#
+# user = "Peter"
+# print(user)
+
+# print(int(3.14))
+
+# print("ë 😂😉")
+
+# import math  => math.pi
+# from math import pi  => pi
+# from math import pi as mathematical_pi  => mathematical_pi
+#
+# print(mathematical_pi)
+# math_pi = 3.141592
+#
+# print(f"My static text here - my dymanic values = {math_pi}")
+
+# print(1 + 3 + 3 + 7)
+# print("1" + "3" + "3" + "7")
+
+# print("My test")
+
+
+# print(int("9.00"))
+#
+# num1 = 100
+# print(type(num1))
+#
+# num1_as_str = str(num1)
+# print(type(num1_as_str))
+#
+# str(input())
+#
+# my_var = input()
+# my_real_num = float(input())
+# my_int_num = int(input())
+
+# my_num_as_string = input()
+# my_int_num = int(my_num_as_string)
+
+# my_input = input()
+# print(my_input)
+# print(type(my_input))
+# print("--------")
+#
+# my_input_as_int = int(my_input)
+# print(my_input_as_int)
+# print(type(my_input_as_int))
+
+# user_age = int(input("Please insert your age here: "))
+
+# ten = 10
+# five = 5
+#
+# print(ten / five)
+# print(int(ten / five))
+
+# num1 = 3.14159  # round(...)
+# print(f"{num1:.2f}")
+#
+# num2 = 3.1  # 3.100000
+# print(f"{num2:.3f}")
+#
+
+# print(f"The final price is: {final_price} lv.\nThe discount is: {money_saved_from_discount} lv.")
+# # print(f"The discount is: {money_saved_from_discount} lv.")
+
+# print("line1\tline3")
+# # print("line2")
+# architect_name = str(input())
+# number_projects = int(input())
+# hours_needed = number_projects * 3
+# print(f"The architect {architect_name} will need {hours_needed} hours to complete {number_projects} project/s.")
+#
+#
+# print(f"the sum is {300 * 22 + 15}")
+
+# architect_name = str(input())
+# number_projects = int(input())
+# # hours_needed = number_projects * 3
+# print(f"The architect {architect_name} will need {number_projects * 3} hours to complete {number_projects} project/s.")
