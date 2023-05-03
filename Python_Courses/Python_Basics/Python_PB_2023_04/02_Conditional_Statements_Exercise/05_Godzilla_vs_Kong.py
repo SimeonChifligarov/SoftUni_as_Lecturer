a85c7f88460867b3578c3a0a269cdd7de9346455
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-03 19:48:44 +0300

    adding 05_Godzilla_vs_Kong.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/02_Conditional_Statements_Exercise/05_Godzilla_vs_Kong.py
@@ -0,0 +1,20 @@
+# DECOR_AS_MULTIPLIER = 0.10
+
+budget = float(input())
+walk_on_count = int(input())
+clothes_price_per_walk_on = float(input())
+
+decor = budget * 0.10
+if walk_on_count > 150:
+    clothes_price_per_walk_on *= 0.90
+    # clothes_price_per_walk_on = clothes_price_per_walk_on * 0.90
+
+money_needed = decor + walk_on_count * clothes_price_per_walk_on
+money_left = budget - money_needed
+MESSAGE_FOR_BUDGET_ENOUGH = f"Action!\nWingard starts filming with {money_left :.2f} leva left."
+MESSAGE_FOR_BUDGET_NOT_ENOUGH = f"Not enough money!\nWingard needs {money_needed - budget :.2f} leva more."
+
+if money_needed > budget:
+    print(MESSAGE_FOR_BUDGET_NOT_ENOUGH)
+else:
+    print(MESSAGE_FOR_BUDGET_ENOUGH)
