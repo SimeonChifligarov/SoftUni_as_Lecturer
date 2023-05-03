75dcaa0fc65f4a172eb54458592b76558bddff0e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-03 18:52:23 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Preparation/01_First_Steps_In_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,11 @@
+import math
+from math import pi
+from math import pi as mathematical_pi
+
+my_num = math.pi
+my_library = math
+
+my_num_2 = pi
+my_num_3 = mathematical_pi
+print(my_num_3)
+# print(my_library)
