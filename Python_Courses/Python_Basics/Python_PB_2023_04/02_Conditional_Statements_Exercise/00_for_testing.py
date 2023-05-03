919bf0db034a87b46a8a3b5521c65c5b79b73783
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-03 19:49:19 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/02_Conditional_Statements_Exercise/00_for_testing.py
@@ -0,0 +1,173 @@
+# print('it is working fine')
+
+# true = "2.71"
+# is_valid = True
+# is_valid = true
+
+# print(is_valid)
+# print(type(is_valid))
+#
+# variable = ""
+# is_variable_not_empty = bool(variable)
+#
+# print(is_variable_not_empty)
+
+# num_as_string = "5"
+# print(type(num_as_string))
+#
+# num = float(num_as_string)
+# print(num)
+# print(type(num))
+
+# is_true = True
+#
+# print(is_true)
+# print('---')
+# print(not is_true)
+
+# str_1 = 12
+# str_2 = 13
+#
+# if not str_1 > str_2:  # code readability
+#     print(str_1 != str_2)
+#     print('they are not equal')
+
+# print("my sta"
+#       "tement")
+
+# if 5 > 3:
+#     print('it is')
+
+# is_...
+# are_...
+
+# print("my text" * 3)
+
+# num_1 = 27
+# print(f"{num_1 :08d}")
+# import math
+#
+# print(math.ceil(2.71828))
+
+# my_int_num = 2023
+#
+# print(my_int_num % 1000)
+#
+
+# input_text = input()
+# if input_text == "":
+#     input_text = "Ivan"
+
+# my_var = 12
+#
+# bonus_points = 0
+# if my_var == 13:
+#     bonus_points = 10
+# elif my_var > 100:
+#     bonus_points = 1000
+#
+# print(bonus_points)
+
+# cool_variable = input()
+#
+# my_string = "it is cooool"
+# my_f_string = f"it is {cool_variable}"
+#
+# print(my_string == my_f_string)
+
+# my_long_string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit," \
+#                  " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." \
+#                  " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" \
+#                  " ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " \
+#                  "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " \
+#                  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui" \
+#                  " officia deserunt mollit anim id est laborum."
+#
+# print(my_long_string)
+
+# print("\t" == "    ")
+# if 5 > 4:
+#     print('yess')
+#     print('line1')
+
+# if 5 > 10:
+#     print("3" + 3)
+#
+# print('I am here')
+
+# if False:
+#     my_var = "Simo"
+#
+# print(my_var)
+
+# number = 2023
+# if number > 2000:
+#     print('greater than 2000')
+#     print('cool!')
+# elif number % 2 == 0:
+#     print('is even')
+# else:
+#     print('line1')
+#     print('line2')
+#     print('line3')
+#
+
+# my_name = input()
+
+# if my_name == "Simeon":
+#     print("you are Simeon")
+# else:
+#     print("you are not Simeon")
+
+# print("you are Simeon") if my_name == "Simeon" else print("you are not Simeon")
+
+# my_num = 20
+# # if my_num % 2 == 0:
+# #     my_num = (my_num + 1000)
+# # else:
+# #     my_num = 13
+# # my_num = (my_num + 1000) if my_num % 2 == 0 else my_num = 13
+#
+# print(my_num)
+
+# first_name =
+
+# num = 1000
+# print(num)
+#
+# print('----')
+# # num = num + 20
+# num += 20  # -= /= //= %= *=  # num = num * 10 IDENTICAL WITH num *= 10
+#
+# print(num)
+
+# # if True:
+# print('condition 1')
+# print('line2 from condition1')
+#
+# print('some logic here...')
+#
+# if True:
+#     print('condition 2')
+#
+#
+# if True:
+#     print('here')
+
+# str(), int(), float(), print(), round()
+# print(sum([3, 4, 5, 6]))
+
+# am_or_pm = input("please enter if 'am' or 'pm': ")
+# if am_or_pm == "am":
+#     print('it is AM!!!')
+#     # ...
+# elif am_or_pm == "pm":
+#     print('it is PM!!!')
+# else:
+#     print("invalid input data!!!")
+
+
+# print(math.pi)
+
+my_name = "Georgi"
+print(f"my name is {my_name}")
