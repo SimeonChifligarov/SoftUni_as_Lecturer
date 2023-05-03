3da9f38dd33c937856309c38e861bb88e9affea1
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-03 18:44:22 +0300

    adding 07_Projects_Creation.py

---
+++ Python_Courses/Python_Basics/Preparation/01_First_Steps_in_Coding_Lab/07_Projects_Creation.py
@@ -0,0 +1,6 @@
+HOURS_FOR_A_PROJECT = 3
+architect = input()
+projects = int(input())
+hours_needed = projects * HOURS_FOR_A_PROJECT
+
+print(f"The architect {architect} will need {hours_needed} hours to complete {projects} project/s.")
