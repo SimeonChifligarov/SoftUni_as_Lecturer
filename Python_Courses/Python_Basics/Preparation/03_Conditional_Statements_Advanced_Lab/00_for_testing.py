541804f922a26b851614e6f1e965cbb4b0f17f9a
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-15 08:04:57 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Preparation/03_Conditional_Statements_Advanced_Lab/00_for_testing.py
@@ -0,0 +1,21 @@
+# print('it is all configured fine')
+
+# pass = 12
+# if = 13
+# print(if)  # SyntaxError: invalid syntax
+
+# if 5 > 3 or my_undeclared_variable == "TBD":
+#     print('yes')  # prints 'yes'
+#
+# if my_undeclared_variable == "TBD" or 5 > 3:
+#     print('yes')  # NameError: name 'my_undeclared_variable' is not defined
+
+# if 5 < 3 or my_undeclared_variable == "TBD":
+#     print('yes')  # NameError: name 'my_undeclared_variable' is not defined
+
+# if 5 < 3 and my_undeclared_variable == "TBD":
+#     print('yes')  # NameError: name 'my_undeclared_variable' is not defined
+# else:
+#     print('HERE!')
+
+
