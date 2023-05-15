c9e8506159da340b0c8303c23763d64d0552dc5b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-15 07:58:56 +0300

    adding 03_New_House.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/03_Conditional_Statements_Advanced_Exercise/03_New_House.py
@@ -0,0 +1,41 @@
+ROSE_PRICE = 5.00
+DAHLIAS_PRICE = 3.80
+TULIP_PRICE = 2.80
+NARCISSUS_PRICE = 3.00
+GLADIOLUS_PRICE = 2.50
+
+flower_type = input()  # "Roses", "Dahlias", "Tulips", "Narcissus" или "Gladiolus"
+flower_count = int(input())
+budget = int(input())
+
+price = 0
+discount = 0
+if flower_type == "Roses":
+    price = ROSE_PRICE
+    if flower_count > 80:
+        discount = 0.10
+elif flower_type == "Dahlias":
+    price = DAHLIAS_PRICE
+    if flower_count > 90:
+        discount = 0.15
+elif flower_type == "Tulips":
+    price = TULIP_PRICE
+    if flower_count > 80:
+        discount = 0.15
+elif flower_type == "Narcissus":
+    # •	Ако Нели купи по-малко от 120 Нарциса - цената се оскъпява с 15%;
+    price = NARCISSUS_PRICE
+    if flower_count < 120:
+        discount = -0.15
+elif flower_type == "Gladiolus":
+    price = GLADIOLUS_PRICE
+    if flower_count < 80:
+        discount = -0.20
+
+total_price = flower_count * price * (1 - discount)  # (1 - (-0.20) ) == (1 + 0.20) == 1.20
+
+if budget >= total_price:
+    print(f"Hey, you have a great garden with {flower_count} {flower_type} "
+          f"and {budget - total_price :.2f} leva left.")
+else:
+    print(f"Not enough money, you need {total_price - budget :.2f} leva more.")
