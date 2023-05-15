59668e8cb476e8a65a60012a02bb9f693a49558f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-15 08:00:36 +0300

    adding 08_On_Time_for_the_Exam.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/03_Conditional_Statements_Advanced_Exercise/08_On_Time_for_the_Exam.py
@@ -0,0 +1,29 @@
+exam_hour = int(input())
+exam_min = int(input())
+arrival_hour = int(input())
+arrival_min = int(input())
+
+exam_time_as_min = exam_hour * 60 + exam_min
+arrival_time_as_min = arrival_hour * 60 + arrival_min
+
+diff = arrival_time_as_min - exam_time_as_min
+if diff > 0:  # arrival_time_as_min > exam_time_as_min
+    print("Late")
+    if diff < 60:
+        print(f"{diff} minutes after the start")
+    else:
+        hh = abs(diff) // 60
+        mm = abs(diff) % 60
+        print(f"{hh}:{mm :02d} hours after the start")
+elif diff >= -30:
+    print("On time")
+    if not diff == 0:
+        print(f"{abs(diff)} minutes before the start")
+else:
+    print("Early")
+    if diff > -60:
+        print(f"{abs(diff)} minutes before the start")
+    else:
+        hh = abs(diff) // 60
+        mm = abs(diff) % 60
+        print(f"{hh}:{mm :02d} hours before the start")
