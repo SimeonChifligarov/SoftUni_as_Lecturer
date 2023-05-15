8ce746ba9d267a1a2682ec2059da0f4ffcd01224
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-15 07:57:12 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/03_Conditional_Statements_Advanced_Exercise/00_for_testing.py
@@ -0,0 +1,110 @@
+# print('just checking...')
+import math
+
+number_1 = 25
+
+# if number_1 > 100 and number_1 > 110 and number_1 < 200 and number_1 < 300:  # True/False
+
+# print(my_undeclared_variable)
+
+# if number_1 > 100 or my_undeclared_variable == "TBD":
+#     print('yes')
+
+# if number_1 < 30 and my_undeclared_variable == "TBD":
+#     print('here')
+# else:
+#     print('now here!')
+
+# if 3 > 2:
+#     print('yezzz')
+
+# day_of_week = input()  # Monday...
+#
+# if day_of_week == "Monday":
+#     print("no1")
+# # elif ...
+# else:  # instead of "elif day_of_week == "Sunday""
+#     print("sunday")
+#
+
+# cost = 100
+# count = 10
+# discount = 0.50
+# extra_discount = 0.50
+#
+# print(cost * count * (1 - (discount + extra_discount)))
+# print(cost * count * (1 - discount) * (1 - extra_discount))
+
+# cost = 100
+# discount = 5 / 100  # 0.05
+#
+# new_cost = cost * (1 - discount)
+# print(new_cost)
+#
+# # new_cost_second_way = cost
+# cost *= 0.95
+# print(cost)
+#
+# print('---')
+# print(cost == new_cost)
+
+
+# number = 100
+#
+# if 50 < number < 200:
+#     print('between 50 and 200')
+
+# print(12 / 0)
+
+# print("my name is Simo")
+
+# my_var = 12
+# print(my_var)
+# my_var = "Peter"
+# print(my_var)
+
+# print(sum([1, 2, 3]))
+# sum = 1337
+# print(sum)
+
+# if 3 > 2:
+#     print('yes')
+
+# and or not if
+
+# if = 13
+# print(if)
+
+# # pass
+# num = 100
+# if num >= 10:
+#     pass
+# else:
+#     print('yes')
+
+# num = int(input())
+#
+# if num > 200:
+#     print('yessss')
+#     print('!!!')
+# elif num > 175:
+#     print('zzz')
+# else:
+#     print('here')
+#
+# print('unconditional print')
+
+# number = 100
+# negative_number = -100
+#
+# print(number == negative_number)
+# print(abs(number) == abs(negative_number))
+
+# TOY_PRICE = get_toy_price(...)
+#
+#
+# check_user_is_logged_in()
+# check_item_is_purchased(TOY_PRICE)
+
+print(f"{0.25 :.0f}")
+print(math.ceil(0.25))
