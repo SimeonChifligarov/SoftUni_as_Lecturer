1e53e18a12a00637cde381567c0450f3a944648d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-24 08:46:50 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/04_For_Loop_Exercise/00_for_testing.py
@@ -0,0 +1,155 @@
+# print('it is all right')
+
+# for number in range(4):
+#     print("yesss!", end="👏\n")
+
+# range()
+#
+# num = int(input())
+#
+# if num > 10:
+#     my_case = "greater than 10"
+# else:
+#     my_case = "not greater than 10"
+#
+# print(my_case)
+
+# for _ in range(5):  # [0, 1, 2, 3, 4]
+#     # print(f"Attempt No: {attempt}")
+#     print("Trying to fetch data...")
+#     # print(_)
+
+# name = input()
+#
+# print(name[3])
+
+# counter = 0
+#
+# for num in range(1, 1001):  # [1, 2, 3, ... , 1000]
+#     counter += 1
+#     print(f"Operation No: {counter}")
+#
+#     if num % 7 == 0:
+#         print(num)
+
+# counter = 0
+#
+# for num in range(7, 1001, 10):  # [7, 17, 27, ..., 997]
+#     counter += 1
+#     print(f"Operation No {counter}")
+#
+#     print(num)
+
+# num = 13_37
+#
+# print(num // 100)
+# print(num % 100)
+
+# num = 100_000_000
+# print(num)
+# print(type(num))
+
+# import sys
+#
+# num1 = float('inf')
+# num2 = sys.maxsize
+# print('Stop here')
+# print(num1 > num2)
+
+
+# break
+
+# MAX_TRIES_COUNT = 100
+#
+# for attempt in range(MAX_TRIES_COUNT):  # [0, 1, 2, 3, 4, ..., 100]
+#     # print(f"Attempt No: {attempt}")
+#     print("Trying to fetch data...")
+#     if attempt == 2:
+#         print("Success")
+#         break
+
+# total_sum = 0
+# for i in range(5):
+#     # print(i)
+#     total_sum += 100
+#     print(total_sum)
+#     if total_sum > 222:
+#         print('will break the for loop')
+#         break
+
+# exit()
+
+# print('line 1')
+# # break
+
+# num = int(input())
+#
+# # if num > 100:
+# #     print("over 100")
+# # else:
+# #     print('under 100')
+#
+# if num <= 100:
+#     print("under 100")
+# else:
+#     print("over 100")
+
+# if not num % 2 == 0:
+
+# for num in range(10):  # index
+#     print('current iteration ' + str(num))
+
+# for num in range(1, 25, 3):
+
+# current_name = ...
+# curr_name = ...
+
+# for _ in range(10):  # index
+#     print('current iteration!')
+
+# my_num = int(input())
+#
+# is_break = False
+# for num in range(5, 25):
+#     print(num)
+#     if num == my_num:
+#         is_break = True
+#         break
+#
+# print('here')
+# if is_break:
+#     print('the for loop has been broken')
+# else:
+#     print('it has finished without been broken')
+
+# if-else:
+# for-else:
+
+# my_num = int(input())
+#
+# for num in range(5, 25):
+#     print(num)
+#     if num == my_num:
+#         print('the for loop has been broken')
+#         break
+# else:
+#     print("it has finished without been broken")
+
+
+# print(f"{2 :100d}")
+# print(f"{222 :100d}")
+# print(f"{22222222 :100d}")
+# print(f"{2 :100d}")
+
+# print(f"{4.3 :.2f}$")
+#
+# n = 5
+# for _ in range(n - 1):  # range(4) => [0, 1, 2, 3]
+#     # next_sum = int(input()) + int(input())
+#     num1 = int(input())
+#     num2 = int(input())
+#     next_sum = num1 + num2
+#     print(next_sum)
+
+# import this
+# import antigravity
