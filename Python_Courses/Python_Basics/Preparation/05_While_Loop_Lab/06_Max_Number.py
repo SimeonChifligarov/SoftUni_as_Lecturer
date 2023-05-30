db91e49c9304bfbade6307652b8215ebec4c9f73
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-30 11:27:25 +0300

    adding 06_Max_Number.py

---
+++ Python_Courses/Python_Basics/Preparation/05_While_Loop_Lab/06_Max_Number.py
@@ -0,0 +1,13 @@
+import sys
+
+max_number = -sys.maxsize
+
+command = input()
+while not command == "Stop":
+    number = int(command)
+    if number > max_number:
+        max_number = number
+
+    command = input()
+
+print(max_number)
