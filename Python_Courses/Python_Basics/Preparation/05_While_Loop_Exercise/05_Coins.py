8b67db207a254c287a6f5af01b5a9a44579dd3d4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-05-30 11:35:50 +0300

    adding 05_Coins.py

---
+++ Python_Courses/Python_Basics/Preparation/05_While_Loop_Exercise/05_Coins.py
@@ -0,0 +1,26 @@
+change_amount = float(input())
+
+change_amount_pennies = int(change_amount * 100)
+total_coins = 0
+
+while change_amount_pennies > 0:
+    if change_amount_pennies >= 200:
+        change_amount_pennies -= 200
+    elif change_amount_pennies >= 100:
+        change_amount_pennies -= 100
+    elif change_amount_pennies >= 50:
+        change_amount_pennies -= 50
+    elif change_amount_pennies >= 20:
+        change_amount_pennies -= 20
+    elif change_amount_pennies >= 10:
+        change_amount_pennies -= 10
+    elif change_amount_pennies >= 5:
+        change_amount_pennies -= 5
+    elif change_amount_pennies >= 2:
+        change_amount_pennies -= 2
+    elif change_amount_pennies >= 1:
+        change_amount_pennies -= 1
+
+    total_coins += 1
+
+print(total_coins)
