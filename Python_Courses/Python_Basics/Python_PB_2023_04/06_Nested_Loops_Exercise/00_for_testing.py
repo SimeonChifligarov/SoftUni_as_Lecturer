d82cbe8e592e43e33a2fe0cd66b1a63e7034bae1
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-04 13:27:05 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_04/06_Nested_Loops_Exercise/00_for_testing.py
@@ -0,0 +1,33 @@
+# print('it works!')
+#
+# for i in range(3):
+#     print(f"current value of i is: {i}")
+#     print("some logic for outer loop ...")
+#
+#     for j in range(5):
+#         print('----')
+#         print(f"current value of j: {j}")
+#         print(f"current value of i is: {i}")
+
+# number = 9751
+# number_as_str = str(number)
+# for _, digit in enumerate(number_as_str):
+#     # print(f"idx value: {idx}")
+#     # print(f"digit value: {digit}")
+#     # print("end of current iteration!")
+#     print("current digit:" + digit)
+
+# num1 = 100
+# for _ in range(10):
+#     for _ in range(100):
+#         while num1 < 100:
+#
+
+# for num in range(100, 1, -2):  # step=1
+#     print(num)
+#     print('---')
+
+# for divisor in range(2, ...):
+#     if num % divisor == 0...
+
+
