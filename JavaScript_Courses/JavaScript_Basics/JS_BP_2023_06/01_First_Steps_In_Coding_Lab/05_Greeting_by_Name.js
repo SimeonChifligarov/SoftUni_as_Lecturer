518a0e1ffb1f00af26f75d2f54f2e44539d0e1c8
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-07 14:10:59 +0300

    adding 05_Greeting_by_Name.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2023_06/01_First_Steps_In_Coding_Lab/05_Greeting_by_Name.js
@@ -0,0 +1,10 @@
+function greetingByName(arrayWithNames) {  // input държи масивът ["Simeon"]
+    let name = arrayWithNames[0];  // name държи "Simeon"
+    let result = `Hello, ${name}!`;
+
+    console.log(result);
+    // console.log(`Hello, ${name}!`);
+}
+
+
+// greetingByName(["Simeon", "Ivan", "Georgi"]);
