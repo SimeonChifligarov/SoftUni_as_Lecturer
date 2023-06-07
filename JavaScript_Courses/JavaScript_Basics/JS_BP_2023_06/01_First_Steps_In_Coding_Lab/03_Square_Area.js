1bda4da557930ce2723c7140a9b2b7c2fe6b6224
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-07 14:09:53 +0300

    adding 03_Square_Area.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2023_06/01_First_Steps_In_Coding_Lab/03_Square_Area.js
@@ -0,0 +1,9 @@
+function squareArea(input) {  // input държи ["5"]
+    let sideA = Number(input[0]);     // sideA държи Number("5") => 5
+    let area = sideA * sideA;  // area държи "5" * "5"
+
+    console.log(area);
+}
+
+
+// squareArea(["5"]);
