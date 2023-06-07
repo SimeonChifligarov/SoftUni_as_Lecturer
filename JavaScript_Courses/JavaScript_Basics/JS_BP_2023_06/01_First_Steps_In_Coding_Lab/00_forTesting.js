f8a84fb85de80f926c59b8cfea3c86ab567ab6ac
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-07 14:07:47 +0300

    adding 00_forTesting.js

---
+++ JavaScript_Courses/JavaScript_Basics/JS_BP_2023_06/01_First_Steps_In_Coding_Lab/00_forTesting.js
@@ -0,0 +1,75 @@
+// console.log("My text goes here")
+// console.log("line 2")
+// console.log("runtime")
+// console.log("My text goes here")
+
+// console.log(13/0)
+
+// let myVariable1 = 13;  // Number
+// let myVariable2 = "13";  // String
+
+// console.log(myVariable + 1000);
+
+// let myVariable3;
+
+// console.log(myVariable3);
+
+// console.log(myUndeclaredVariable);
+
+// let array = [1, 2, 3, 44, 55];
+
+// let element1 = array[0];
+// let element2 = array[1];
+// let element3 = array[2];
+// let element4 = array[3];
+// let element5 = array[4];
+
+// console.log(element1);
+// console.log(element2);
+// console.log(element3);
+// console.log(element4);
+// console.log(element5);
+
+// console.log("5a" * "10");  // NaN -> Not A Number
+
+// let text1 = "text 1"  // String
+// let text2 = 'some text 2'
+// let text3 = `some text ${text1}`  // interpolated string, defined by `
+
+// console.log(text3);
+// console.log("Hello SoftUni");
+
+// PEMDAS
+
+// let result1 = 1 + 5 * 10;
+// let result2 = (1 + 5) * 10;
+
+// console.log(result1);
+// console.log(result2);
+
+// `${var}`
+
+// console.log('here I am');
+// console.log('22');
+
+// let number = 19;
+// // let numberAsString = 10;  // or "10"
+// let number2 = 10;
+
+// // console.log(number + numberAsString);
+// // console.log(number * numberAsString);
+// // console.log(number - numberAsString);
+// // console.log(number / numberAsString);  // math division, i.e. 1.3
+// console.log(parseInt(number / number2));
+// console.log(number % number2);
+
+
+// let num1 = 1337;
+// let num2 = 1332;
+
+// // console.log(num1 % 10);  // 1337 -> 133 х 10; with remainder 7;
+// // console.log(num1 % 1000);
+// console.log(num1 % 2 == 0);
+// console.log(num2 % 2 == 0);
+
+// totalCost = input[0] * price  // -10
