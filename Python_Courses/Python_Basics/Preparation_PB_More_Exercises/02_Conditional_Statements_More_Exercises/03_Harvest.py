c1bfe7cd66ef372ca5b55d2d3db27f7719300209
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-18 11:32:10 +0300

    adding 03_Harvest.py

---
+++ Python_Courses/Python_Basics/Preparation_PB_More_Exercises/02_Conditional_Statements_More_Exercises/03_Harvest.py
@@ -0,0 +1,21 @@
+from math import floor, ceil
+
+area = int(input())
+grapes_per_meter = float(input())
+needed_litres = int(input())
+workers = int(input())
+
+available_area = 0.4 * area
+grapes_for_litre = 2.5
+
+total_grapes = available_area * grapes_per_meter
+wine = total_grapes / grapes_for_litre
+
+if wine < needed_litres:
+    diff = floor(needed_litres - wine)
+    print(f"It will be a tough winter! More {diff} liters wine needed.")
+else:
+    diff = ceil(wine - needed_litres)
+    per_person = ceil(diff / workers)
+    print(f"Good harvest this year! Total wine: {floor(wine)} liters.")
+    print(f"{diff} liters left -> {per_person} liters per person.")
