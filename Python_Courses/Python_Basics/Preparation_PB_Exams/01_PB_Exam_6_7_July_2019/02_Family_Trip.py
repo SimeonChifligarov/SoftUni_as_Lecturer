025e585a8b028936a60d9f7d020d19ef214993ad
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-06-26 11:52:40 +0300

    adding 02_Family_Trip.py

---
+++ Python_Courses/Python_Basics/Preparation_PB_Exams/01_PB_Exam_6_7_July_2019/02_Family_Trip.py
@@ -0,0 +1,17 @@
+budget = float(input())
+nights = int(input())
+night_price = float(input())
+additional_costs = int(input()) / 100
+
+nights_cost = nights * night_price
+if nights > 7:
+    nights_cost *= 0.95
+
+additional_costs = budget * additional_costs
+budget_left = budget
+budget_left -= nights_cost + additional_costs
+
+if budget_left >= 0:
+    print(f"Ivanovi will be left with {budget_left :.2f} leva after vacation.")
+else:
+    print(f"{-budget_left :.2f} leva needed.")
