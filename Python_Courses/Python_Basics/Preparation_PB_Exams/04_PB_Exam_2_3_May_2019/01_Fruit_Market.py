a0f702ed65edfa01b062c06c90364d22548f9d56
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-07-01 11:02:05 +0300

    adding 01_Fruit_Market.py

---
+++ Python_Courses/Python_Basics/Preparation_PB_Exams/04_PB_Exam_2_3_May_2019/01_Fruit_Market.py
@@ -0,0 +1,16 @@
+strawberries_price = float(input())
+bananas_kg = float(input())
+oranges_kg = float(input())
+raspberries_kg = float(input())
+strawberries_kg = float(input())
+
+raspberries_price = strawberries_price * 0.50
+oranges_price = raspberries_price * 0.60
+bananas_price = raspberries_price * 0.20
+
+total_price = strawberries_kg * strawberries_price \
+              + bananas_kg * bananas_price \
+              + oranges_kg * oranges_price \
+              + raspberries_kg * raspberries_price
+
+print(f"{total_price :.2f}")
