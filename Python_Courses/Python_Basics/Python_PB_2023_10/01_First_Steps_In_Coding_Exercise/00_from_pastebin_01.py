3dc6e9a30ef30f5bc1535af97454a49c0deb5118
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-03 10:24:55 +0300

    adding 00_from_pastebin_01.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/01_First_Steps_In_Coding_Exercise/00_from_pastebin_01.py
@@ -0,0 +1,8 @@
+chicken = int(input()) * 10.35
+fish = int(input()) * 12.40
+other = int(input()) * 8.15
+total_price_for_menu = chicken + fish + other
+dessert = total_price_for_menu * 0.2
+percent = 2.50
+total = total_price_for_menu + dessert + percent
+print(total)
