bab911729ed34980f369fde8a3145086b38cc6b4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-03 10:24:34 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/01_First_Steps_In_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,82 @@
+# print("it works!")
+
+# print(name)
+
+# name = "Simeon"  # 'Simeon'
+# age = 22
+# weight = 66.0
+
+# print(type(weight))
+
+
+# print('here is the input ' + name)
+# print(type(name))
+
+# print("Hell0")
+# import math
+#
+# math.pi
+
+# print(1300 + 37)
+# name = 12
+#
+# print("name")
+# print("===========")
+# print(name)
+
+# print(nam)
+
+# usd = int(input())
+
+# print("bgn" + 137)
+
+# USER = "Simeon"
+# print(USER)
+#
+# USER = 13.37
+# # print(USER)
+#
+# my_number = 1
+# print(my_number)
+#
+# my_number = 1_000_000_000_000_000_000_000_000_000_000_000
+# print(my_number)
+#
+
+# number = int(input())
+
+# print(10 / 5)  # 2.0
+# print(type(10 / 5))
+
+# print(11 // 5)  # 5 x 2 = 10; reminder 1
+# print(11 % 5)
+
+# age = float(input())
+#
+# print(age)
+
+# number_of_shoes_bought = int(input())
+import math
+
+# print(math.pi)
+# print(math.e)
+
+# number = 3.11
+#
+# print(round(number))
+# print(math.floor(number))
+# print(math.ceil(number))
+
+# print(0.1 + 0.2)
+
+# result = 73.5
+# print(f"{result :.2f}$")
+# print(round(result, 2))
+
+# TypeError: can't multiply sequence by non-int of type 'float'
+
+text = float("142")  # 142.0
+times = 3.2
+
+# print(text + " ! " + text)
+print(text * times)
