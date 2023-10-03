009fe426a0bc88cb6c4576a382b4e43031799f2e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-03 10:26:13 +0300

    adding 04_Vacation_books_list.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/01_First_Steps_In_Coding_Exercise/04_Vacation_books_list.py
@@ -0,0 +1,9 @@
+total_pages = int(input())
+pages_per_hour = int(input())
+days = int(input())
+
+total_hours_needed = total_pages // pages_per_hour
+
+hours_per_day_needed = total_hours_needed / days
+
+print(int(hours_per_day_needed))
