fddf0b0f93749707c23ae8241b61e88a5b33f921
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-03 10:26:54 +0300

    adding 08_Basketball_Equipment.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/01_First_Steps_In_Coding_Exercise/08_Basketball_Equipment.py
@@ -0,0 +1,9 @@
+annual_fee = int(input())
+basket_shoes = annual_fee * 0.60
+basket_uniform = basket_shoes * 0.80
+basket_ball = basket_uniform * 0.25
+basket_accessories = basket_ball * 0.20
+
+total_cost = annual_fee + basket_shoes + basket_uniform + basket_ball + basket_accessories
+
+print(total_cost)
