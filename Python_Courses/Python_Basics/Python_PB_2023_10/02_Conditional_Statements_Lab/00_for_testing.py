3e85665c1b52d4efd809d2c89a46996c1e7d7113
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-09 11:39:00 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/02_Conditional_Statements_Lab/00_for_testing.py
@@ -0,0 +1,205 @@
+# print('it works 😍, наистина')
+#
+# print("Code executes here...")
+#
+# print("Code 2...")
+# print("Code 3...")
+# print("Code 4...")
+
+# print("Make coffee!")
+#
+# print("If it rains:")
+# print("Take an umbrella")
+#
+# print("It it doesn't:")
+# print("Take sunglasses")
+#
+# print("Go to work!")
+
+# print("a" + "b")  # "ab"
+# print(1 + 3)  # 4
+#
+# print("1" + "3")  # "13"
+
+# int(input()) ; float(input())
+
+# number = "1000"
+# print(type(number))
+
+# var1 = 13  # type == int
+# var2 = "13"  # type == str
+#
+# print(var2)
+#
+# var2 = 1444
+
+# print(10 % 3)  # 10 // 3 = 3; 10 -> 9 3x3
+
+# print(1 + 1 + "4" + 2 + 1)
+# num1 = 10_001_337.1373
+# num2 = 22.22222
+# print(f"{num1 :20,.2f}")
+# print(f"{num2 :20,.2f}")
+
+# num3 = 1337.1373
+# print(f"{num3 :.3f}")
+
+# print()
+
+# print(13 >= 10)
+# print(13 >= 13)
+
+# print(13 => 10)
+
+# my_number = input()
+#
+# print(my_number >= 18)
+
+# a = 5
+# b = 10
+# print(a < b)
+# print(a > 0)
+# print(a > 100)
+# print(a < a)
+# print(a <= 5)
+# print(b == 2 * a)  # 10 == 10, i.e. True
+
+# is_bigger = 10 > 5
+# print(is_bigger)
+# print(type(is_bigger))
+
+# print(False < True)  # 0, 1  => 2, => 0, 1
+# print(False == 0)
+# print(bool(0))
+#
+# is_rainy = input()
+#
+# print("Make coffee!")
+#
+# # print("If it rains:")
+# if is_rainy == "yes":
+#     print("Take an umbrella")
+#
+#     print("Make action 2 only in if-block-code")
+#
+# print("It it doesn't:")
+# print("Take sunglasses")
+#
+# print("Go to work!")
+#
+# # print("hello")
+
+# if 3 > 2:
+#     some code here...
+#
+#
+#
+
+# if 10 > 5:
+#     print("yes")
+
+# print("Same" == "same")  # case-sensitivity
+# print("Same" == "Same")  # case-sensitivity
+
+# number_1 = 13  # snake_case
+# Number_1 = 14
+# NumBer_1 = 15
+#
+# print(Number_1)
+
+# age = int(input())
+# if age < 18:
+#     print("You are a minor")
+# elif 18 <= age < 65:  # int(int) => int;
+#     print("You are an adult")
+# else:
+#     print("You are a senior citizen")
+
+# "CaSeInt" => "caseint"
+
+# ASCII
+
+# "A" => ...
+# "a" => ...
+
+# print(ord("A"))  # ASCII
+# print(ord("a"))
+
+# up = math.ceil(23.45)
+# import math
+#
+# number = 3.1415
+#
+# round_number = round(number, 2)
+# print(round_number)
+#
+# up = math.ceil(number)
+# print(up)
+# # print(math.ceil(3.0000))  # 3
+#
+# down = math.floor(number)
+# print(down)
+
+# weather = input()
+#
+# print("Make coffee!")
+#
+# if weather == "rain":
+#     print("Take an umbrella")
+# elif weather == "snow":
+#     print("yesss, it is snowing")
+#     print("Take snow boots")
+# else:
+#     print("Take sunglasses")
+
+# print("Go to work!")
+
+
+# current_day = input()
+#
+#
+# # salary = 0
+# if current_day == "Monday":
+#     salary = 1000
+# else:
+#     salary = 2000
+#
+# print(salary)  # Error
+# # print(salary + 100)
+
+# number = int(input())
+#
+# if number < 100:
+#     print("Less than 100")
+#
+# if 100 <= number <= 200:  # number >= 100 and number < 200:  # if number is between 100 & 200
+#     print("Between 100 and 200")
+# elif number > 200:
+#     print("Greater than 200")
+#
+#
+#
+#
+#
+# day_of_week = input()
+#
+# if day_of_week == "Mon":
+#     price_1 = int(input())
+#     price_2 = int(input())
+#     print(price_1 + price_2)
+# else:
+#     price_3 = int(input())
+#     print(price_3)
+
+
+# age = int(input("Please, insert your age here: "))
+# name = input("Please, insert name, 😎: ")
+#
+# print(f"Hi, {name}, your age is {age}")
+
+
+# name_and_age = input()
+# print(name_and_age)
+
+print(bool("circle"))
+print(bool(""))
