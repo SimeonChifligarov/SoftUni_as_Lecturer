3b1ac1070b011cbdf8004c88fa322f848aaa76ba
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-10 10:38:09 +0300

    adding 00_from_pastebins_003.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/02_Conditional_Statements_Exercise/00_from_pastebins_003.py
@@ -0,0 +1,17 @@
+number = int(input())
+bonus = 0
+
+if number <= 100:
+    bonus = 5
+elif number > 1000:
+    bonus = number * 0.10
+else:
+    bonus = number * 0.20
+
+if number % 2 == 0:
+    bonus = bonus + 1
+elif number % 10 == 5:
+    bonus = bonus + 2
+
+print(bonus)
+print(bonus + number)
