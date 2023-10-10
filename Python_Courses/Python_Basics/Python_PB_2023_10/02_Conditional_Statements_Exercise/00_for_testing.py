5fdccba843e98679b323714c1d923009123f78b4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-10 10:37:17 +0300

    adding 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/02_Conditional_Statements_Exercise/00_for_testing.py
@@ -0,0 +1,103 @@
+# print("it workzzz!")
+
+
+# i = 33
+# print(f'{i:02d}')
+
+# is_valid = True
+# num = int(input("Please insert a number between 1 and 50: "))
+# if num < 1:
+#     print("Sorry, invalid input")
+#     is_valid = False
+# if num > 50:
+#     print("Sorry, invalid input")
+#     is_valid = False
+#
+# if is_valid:  # True == True;  if variable:
+#     print(f"Yess, valid number {num}")
+
+# print(10 / 3)
+# print(10 // 3)  # 3
+# print(10 % 3)   # 1
+
+# string = input()
+#
+# if string:
+#     print('it is not empty')
+# else:
+#     print(f'here we are, with the empty string - !{string}!')
+
+# number = 1337
+#
+# print(number % 10)
+# print(number % 100)
+# print(number % 1000)
+#
+# number_two = 10_000
+# print(number_two % 100)
+
+# number_14 = 14
+# number_15 = 15
+#
+# print(number_14 % 10)
+# print(number_14 % 10 == 5)
+# print('------')
+# print(number_15 % 10)
+# print(number_15 % 10 == 5)
+#
+# if users >= 1_000_000:  # eq 1000000
+#     pass
+#
+# "1_000_000" != "1000000"
+
+# num = 100
+#
+# print(num)
+#
+# num += 13  # num = num + 13
+#
+# print(num)
+
+# print(12 % 2)  # 0 => 12 % 2 == 0  => 0 == 0: True
+# print(13 % 2)  # 1 => 13 % 2 == 0  => 1 == 0: False
+
+# name = input()
+
+# print(f"Hello, {name},", end="")
+# print("nice to meet you!")
+
+# print(f"Hello, {name},\t\t\t\tnice to meet you!")
+
+# print("Hello,\nSimo")
+
+# if name == "Kiril":
+#     print(f"yesss, Kiril")
+# else:
+#     print(f"Sorry, not Kiril - {name}")
+
+# print(f"yesss, Kiril") if name == "Kiril" else print(f"Sorry, not Kiril - {name}")
+
+# number = 12
+#
+# print(number)
+#
+# # if number % 2 == 0:
+# #     number = 100
+# # else:
+# #     number = 1337
+# number = 100 if number % 2 == 0 else 1337
+#
+# print(number)
+
+# print("1" + "333" + 33)
+
+# age = 33
+# print("My name is Simo" + " and my age is " + str(age))
+# print(type(age))
+# print(type(str(age)))
+
+# print(f"My name is Simo, {age}")  # str(age)
+
+number = float("133.22")
+print(number)
+
