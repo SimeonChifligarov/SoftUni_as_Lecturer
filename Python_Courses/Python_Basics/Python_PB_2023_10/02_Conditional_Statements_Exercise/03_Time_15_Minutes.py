cad5b6b95e917932752a4af476672655cca312eb
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-10 10:39:23 +0300

    adding 03_Time_15_Minutes.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/02_Conditional_Statements_Exercise/03_Time_15_Minutes.py
@@ -0,0 +1,30 @@
+#
+### SOLUTION 1
+#
+# current_hour = int(input())
+# current_minute = int(input())
+#
+# current_time_in_minutes = current_hour * 60 + current_minute + 15
+#
+# current_time_as_hour = current_time_in_minutes // 60
+# if current_time_as_hour == 24:
+#     current_time_as_hour = 0
+#
+# current_time_as_minutes = current_time_in_minutes % 60
+#
+# print(f"{current_time_as_hour}:{current_time_as_minutes:02d}")
+
+### SOLUTION 2
+
+current_hour = int(input())
+current_minute = int(input())
+
+current_minute_plus_15 = current_minute + 15
+if current_minute_plus_15 >= 60:
+    current_minute_plus_15 -= 60  # current_minute_plus_15 = current_minute_plus_15 - 60
+    current_hour += 1  # current_hour = current_hour + 1
+
+if current_hour == 24:
+    current_hour = 0
+
+print(f"{current_hour}:{current_minute_plus_15:02d}")
