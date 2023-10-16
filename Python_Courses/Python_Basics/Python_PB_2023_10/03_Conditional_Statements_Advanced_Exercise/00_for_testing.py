524d5b73a9482d329a161f078d39c7460c1d2c9c
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-16 11:07:20 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/03_Conditional_Statements_Advanced_Exercise/00_for_testing.py
@@ -0,0 +1,135 @@
+# print('test if')
+
+# number = 100
+#
+# if number > 45:
+#     print("greater than 45")
+#     if number > 180:
+#         print("greater than 180")
+#
+#
+# [pseudocode]
+# condition_1 -> value; simple check;
+# 			-> complex check;
+
+# [2] simple check
+# number = 10
+# if number > 15:
+#     print("greater than 15")
+
+# [1] value
+# number = 0
+# print(bool(number))
+#
+# if number:
+#     print("number is not 0")
+
+# # text = "my text"
+# text = ""
+# print(bool(text))
+# if text:
+#     print("text is not an empty string")
+
+# [3] complex check
+# number = 100
+#
+# if 50 < number < 60:
+#     print("number is between 50 and 200")
+
+# number = 100
+#
+# # if not number > 50:  # if not var == "yes":  eq if var != "yes":
+# if number <= 50:
+#     print('here')
+
+# a = 5
+# if a == 5 or a == 4 and a == 3:
+#     print('here')
+# else:
+#     print('false')
+
+# a = 15
+# if a == 5 or b == 4 and b == 3:
+#     print('here')
+# else:
+#     print('false')
+
+# print(abs(-100))
+
+# a = 100
+# b = 200
+# diff = a - b
+#
+# print(abs(diff))
+
+# a = 50
+
+# number = 110
+#
+# # if number > 100:
+# #     if number < 200:
+# #         print("number is between 100 and 200")
+#
+# if 100 < number < 200:
+#     print("number is between 100 and 200")
+
+# name = input()
+#
+# # if name != "Simo":  # "Petar" != "Simo"
+# #     print('not Simo')
+#
+# if not name == "Simo":
+#     print('not Simo')
+
+# budget = 1000
+# season = input()
+# discount = 0
+#
+# print(bool(season))
+# # print(bool("Winter"))
+#
+# if season == "Summer" or season == "Winter":
+#     print("new discount = 50%")
+#     discount = 0.50
+
+# price = 100
+# # discount = 0.50  # eq discount = 5 / 100
+#
+# price = price * 0.50
+# print(price)
+
+# print(price * (1 - discount))
+# new_price = price - price * discount
+# print(new_price)
+
+# price = 100
+# discount = 0.50
+# extra_discount = 0.50
+#
+# print(price * (1 - (discount + extra_discount)))
+# print(price * (1 - discount) * (1 - extra_discount))
+# current_price = price * (1 - discount)
+# total_price = current_price * (1 - extra_discount)
+# print(total_price)
+
+# print(type("Yes"))
+
+# num = 2
+#
+# print(num)
+#
+# num **= 10
+#
+# print(num)
+
+# print(30 % 0)
+
+# pass
+
+# num = 100
+# if num > 0:
+#     pass
+# elif num > 10:
+#     pass
+# else:
+#     pass
