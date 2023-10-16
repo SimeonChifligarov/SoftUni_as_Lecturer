4c375fb84a36c834e71d827856d5950d5394b63a
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-16 11:09:29 +0300

    add 06_Operations_Between_Numbers.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/03_Conditional_Statements_Advanced_Exercise/06_Operations_Between_Numbers.py
@@ -0,0 +1,40 @@
+num1 = int(input())
+num2 = int(input())
+math_operator = input()  # "+", "-", "*", "/", "%"
+
+if math_operator == "+":
+    result = num1 + num2
+    even_or_odd = ""
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+    print(f"{num1} {math_operator} {num2} = {result} - {even_or_odd}")
+elif math_operator == "-":
+    result = num1 - num2
+    even_or_odd = ""
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+    print(f"{num1} {math_operator} {num2} = {result} - {even_or_odd}")
+elif math_operator == "*":
+    result = num1 * num2
+    even_or_odd = ""
+    if result % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+    print(f"{num1} {math_operator} {num2} = {result} - {even_or_odd}")
+elif math_operator == "/":
+    if num2 == 0:
+        print(f"Cannot divide {num1} by zero")
+    else:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result :.2f}")
+elif math_operator == "%":  # else:
+    if num2 == 0:
+        print(f"Cannot divide {num1} by zero")
+    else:
+        result = num1 % num2
+        print(f"{num1} % {num2} = {result}")
