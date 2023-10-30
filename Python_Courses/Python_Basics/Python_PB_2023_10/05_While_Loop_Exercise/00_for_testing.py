7c630a148a99e024f983eff672f2594f2f457185
SimeonChifligarov <simeon.chifligarov@gmail.com>
2023-10-30 12:20:50 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2023_10/05_While_Loop_Exercise/00_for_testing.py
@@ -0,0 +1,57 @@
+# print('yes, it works')
+
+# for attempt in range(1, 6):  # [1, 2, 3, 4, 5]
+#     print(f"Attempt No {attempt}")
+#     print("Trying to connect to DB...")
+
+
+# while not is_connected:
+#     try_to_connect
+
+# number = 13
+#
+# while number < 200:
+#     print(f"Current number {number}")
+#
+#     number += 10
+
+# number = 1337
+# while number <= 2000:
+#     print('less than 2000')
+#
+#     if number % 2 == 0:
+#         break
+#
+#     print('will never be printed')
+#
+#
+#     number += 13
+
+# name = input()
+# age = int(input())
+#
+# if name == "Simo" and age == 13:
+#     print(f"Average score: 5.60\nNumber of problems: 5\nLast problem: IronMan")
+# elif name == "Vanko" and age == 15:
+#     print(f"You need a break, 2 poor grades.")
+
+# num1 = 0.1 + 0.2
+# num2 = 0.3
+#
+# print(num1 == num2)
+# print(num1)
+
+# operators -> break AND continue
+
+# name = input()
+#
+# while name != "Simo":
+#     print(f"Hello, {name}")
+#
+#     if name == "XXX":
+#         name = input()
+#         continue
+#
+#     print(f"You are not Simo OR XXX")
+#
+#     name = input()
