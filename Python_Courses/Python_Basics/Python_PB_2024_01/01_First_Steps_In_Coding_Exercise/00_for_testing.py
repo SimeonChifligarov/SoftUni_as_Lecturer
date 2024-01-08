da937db9bbc5b0d54344b5176dd6f03dc4530eb0
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-01-08 13:52:00 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_01/01_First_Steps_In_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,83 @@
+# print("I am here")
+# print("Аз съм готин 😎✅")
+
+# name = 'Stefan'
+# age = 27
+# weight = 87.750
+#
+# print(name)
+# print(type(name))
+# print('=========')
+# print(age)
+# print(type(age))
+# print('=========')
+# print(weight)
+# print(type(weight))
+# # type(name)  # <class 'str'>
+
+# print(name)
+#
+# name = input()
+# age = int(input())  # int('26') => 26
+#
+# print(name)
+# print(age + 1)
+
+# ETL =>
+# 1. Extract
+# 2. Transform
+# 3. Load
+
+# import math
+#
+# print(math.pi)
+
+# name = 'Stefan Trzv'
+# age = 27
+#
+# # print('name:')
+# # print(name)
+# print(f'name: {name}!!!')
+
+
+# print(int('13') + float('37.0'))
+
+# print(int(float('12.8')))
+
+# num = round(13.0000, 3)
+# print(num)
+#
+# print(f'{num :.2f}$')
+
+# print("I'm here")
+
+# print(10 / 2)
+# print(type(9 / 3))
+#
+# print(float('3'))
+# print(float('3.0'))
+# print(int('3'))
+# print(int('3.0'))
+# import math
+#
+# help(math.floor)
+
+# num = 10.500001
+#
+# print(num)
+#
+# num_rounded = round(num)
+# print(num_rounded)
+
+# math.ceil AND math.floor
+# import math
+#
+# math.pi
+# prize = 22
+# print(f'{prize :.2f}$')
+#
+# number = 10000000000000 + 10000000000000 + 10000000000000 + 10000000000000 + 10000000000000 + 10000000000000 \
+#          + 10000000000000 + 10000000000000 + 10000000000000 + 10000000000000
+
+print(sum([1, 2, 3]))
+
