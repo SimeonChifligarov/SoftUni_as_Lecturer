78745c5c7a45ec85f3bf9201c42d3c6001e01432
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-01-14 16:35:50 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_01/02_Conditional_Statements_Exercise/00_for_testing.py
@@ -0,0 +1,137 @@
+# print('I\'m here 😎')
+#
+# # num = 12
+# #
+# # print(type(num))
+# #
+# # num_as_int = int(num)
+# # print(type(num_as_int))
+#
+# # 1. assignment
+# is_valid = True
+#
+# # 2. bool(var)
+# var = ""
+# is_not_empty = bool(var)
+#
+# # print(is_not_empty)
+#
+# # 3. run-time evaluation => comparison operators
+# budget = 1000
+# needed_money = 750
+#
+# is_budget_enough = budget >= needed_money
+# # print(is_budget_enough)
+
+# number_100 = 100
+#
+# if number_100 <= 1000:
+#     print('it is less or equal than 1000')
+# elif number_100 < 500:
+#     print('it is less than 500')
+#     # print('I AM HERE!@#')
+#
+# print("case independent if's")
+#
+# if number_100 < 1000:
+#     print('it is less than 1000')
+# if number_100 < 500:
+#     print('it is less than 500')
+
+# print('here')
+#
+# logger
+#
+# if Logger.LEVEL == "DEBUG":
+#     logger.print_in_yellow
+# elif Logger.LEVEL == "INFO":
+
+# number = 1000
+#
+# if number <= 100:
+#     print('it is not bigger')
+# else:
+#     print('it is bigger')
+
+# if number > 100:
+#     print('it is bigger')
+# else:
+#     print('it is not bigger')
+
+# number = 1234567
+# print(f"{number:05d}")
+
+# print(10 / 3)
+# print(10 // 3)
+
+# if True:
+#     print('yes 1')
+#     print('yes 1')
+
+# number = 12
+#
+# print(f'{number :04d}')
+
+# number = int(input())
+#
+# if number < 100:
+#     print('it is less than 100')  # number E (-inf, 100)
+# elif number < 200:  # number >= 100 and number < 200
+#     print('it is less than 200')  # number E [100; 200)
+# elif number < 300:
+#     print('it is less than 300')  # number E [200; 300)
+
+
+# number = 100
+# print(number)
+# print('some code here...')
+#
+# # number = number + 12
+# number += 12
+# print(number)
+
+# print(1)
+# print()
+# print(2)
+
+# print('my string\nline2')
+
+# a = 100
+# b = 3
+#
+# a += b  # a += 3  <=> a = a + 3
+# print(a)
+#
+# a *= b  # a = 103 * 3
+# print('-------')
+# print(a)
+#
+# a -= b
+# print('-------')
+# print(a)
+#
+# a /= b
+# print('-------')
+# print(a)
+
+# a = 'Simo'
+# b = 'cool'
+#
+# a += b
+# print(a)
+#
+# a *= 3
+# print(a)
+
+# import math
+#
+# print(math.ceil(3.14))
+# print(math.ceil(3.00))
+# print(math.floor(2.99))
+
+budget = float(input())
+walk_on_count = int(input())
+clothes_cost_per_walk_on = float(input())
+
+print('success')
+
