47d84758bb04b7c40903ca32b949faa7fe358d71
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-02-04 13:25:08 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_01/05_While_Loop_Exercise/00_for_testing.py
@@ -0,0 +1,86 @@
+# print('it works!!')
+
+# for attempt in range(1, 6):  # [1, 2, 3, 4, 5]
+#     print(f'Attempt No {attempt}')
+#     database_name = f'DATABASE{attempt}'
+
+# is_connected
+# while not is_connected:
+#     try_to_connected
+# counter = 0
+
+# is_connected = False
+# while not is_connected:
+#     counter += 1
+#     if counter == 5:
+#         is_connected = True
+#
+#     print(f'Attempt No {counter}')
+#     database_name = f'DATABASE{counter}'
+#
+
+# number = 13  # 1
+#
+# while number < 200:  # 2
+#     print(f'number value is {number}')
+#
+#     number += 10  # 3
+
+# for number in range(5):
+#     if number == 2 or number == 4:
+#         print('it will continue')
+#         continue
+#     print(f'iteration {number}')
+
+# start = int(input())
+# end = int(input())
+# for num in range(start, end):
+#     print('here')
+
+# print(10_000 + 3)
+# print(type(13_300_125))
+
+# number = int(input())
+
+# num1 = 0.1
+# num2 = 0.2
+#
+# result = num1 + num2
+#
+# print(f'{result :.2f}')
+#
+# print('-----')
+# print(result)
+#
+# result = float(f'{result :.2f}')  # float('0.30')  == 0.3
+# print(f'result after: {result}')
+
+# number = 9.87654
+#
+# print(round(number, 2))
+
+# print(round(0.35))
+
+# import antigravity
+
+# името_ми = input()
+#
+# годините_ми = int(input())
+# print(f'{името_ми}, на {годините_ми} години')
+
+# turtle  GUI
+
+# clear_string = "\n"
+#
+# while True:
+#     name = input('Give your name here: ')
+#     print(f'Hello, {name}')
+#     print(clear_string * 35)
+
+# iterations = int(input('Колко пъти да четем двойка входове? '))
+# for i in range(iterations):
+#     current_number1 = int(input())
+#     current_number2 = int(input())
+
+# text = 'Simo'
+# print(text.isalpha())
