d04271858545ea7dbf476626ee9ae03582e6d35e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-06-02 10:25:16 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/01_First_Steps_in_Coding_Lab/00_for_testing.py
@@ -0,0 +1,104 @@
+# print('from 01_folder')
+
+# name = 'Simo'  # "Simo"
+# age = 25  # int
+# # age = '25' -> type str
+# height = 1.83  # float
+#
+# print(name)
+# print(type(name))
+# print('---')
+# print(age)
+# print(type(age))
+# print('---')
+# print(height)
+# print(type(height))
+
+# age = 25.0
+# print('as float:')
+# print(type(age))
+
+# age = '25'
+# print(age)
+# print(type(age))
+
+# name = input()  # 'SimeonC'
+# age = input()  # '24'
+# kg = input()  # '75.5'
+#
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+
+# age = input()
+# # print(age)
+# # age = 26
+# # print(age + 1)
+#
+# print(type(age))
+# age = int(age)
+# print(type(age))
+
+# print(3000)
+# print('3001')
+
+# age = int(input())  #
+# print(2 + 5.5)
+#
+# print('2' + '5.5')  # concatenation;
+# age = 22
+# print('Simeon, age:' + str(age))
+# age = 33
+# print(f'Hello, your double age is: {age * 2}!')
+
+# print('Hello' + ' ' + 'Stefan')
+# print('Hello' + 'Stefan')
+# print('Hello ' + 'Stefan')
+
+# car = 'mazda'
+# car = car + 'CX-5'
+# print(car)
+
+# result_1 = (3 + 2) * 5
+# print(result_1)
+#
+# result_2 = 3 + 2 * 5
+# print(result_2)
+
+# float_number = input()
+# int_number = int(float_number)
+# print(int_number)
+
+# name = 'Simo'
+#
+# a = 10
+# b = a + 5
+# c = a + 15
+# d = a + 25
+
+# import math
+#
+# print(math.floor(3.99))
+# print(math.ceil(3.99))
+
+# print('Print Кирилица 😎✔')
+# import math
+
+# math.pi
+
+# import = 'simo'
+
+# print(int('33'))
+#
+# total_sum = 22 + 2
+
+# ''
+
+# import math
+
+# import my_ultra_giga_math
+
+# GUI
+
+import turtle
