f9d60b2980cdfefaeb03a9c61811263b79b11116
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-06-08 15:13:27 +0300

    ádd 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/02_Conditional_Statements_Lab/00_for_testing.py
@@ -0,0 +1,207 @@
+# print('Hello SoftUni')
+
+# number = 1 + '2'
+
+# memory_limit = 15  # int
+# age = 22
+# Age = 24
+# aGe = 33
+#
+# print(age)
+
+# name = 13
+# print(type(name))
+#
+# name_as_string = str(name)  # '13'
+# print(type(name_as_string))
+
+# empty_string = ''
+# print(f'Before{empty_string}After')
+# print(type(empty_string))
+
+# age = int(input())  # int('13') == 13
+# print(type(age))
+
+# user = input('Please insert your name: ')
+# if len(user) == 0:
+#     exit(5)
+
+# name = ''
+#
+# print(f'!{name}!')
+# name = 13
+# print(f'!{name}!')
+# print(type(name))
+
+# print(1 + '1')
+
+# name = '13'  # str
+# age = 13  # int
+# print(1 + 1 + "4" + 2 + 1)
+
+# name = 'Simo'
+#
+# ==
+
+# string_1 = 'Lex'
+# string_2 = 'Lex'
+# string_3 = 'Peter'
+
+# print(string_1 == string_2)  # True
+# print(string_1 != string_2)  # False
+# print(string_1 == string_3)  # False
+
+# print(string_1 > string_3)  # False
+
+# number_1 = 13
+# number_2 = 15
+#
+# print(number_1 == number_2)
+# print(number_1 != number_2)
+# print(number_1 >= number_2)
+# print(number_1 > number_2)
+
+# bool = { True, False }
+# a = 5
+# b = 10
+# print(a < b)
+# print(type(a < b))
+# print(a > 0)
+# print(a > 100)
+# print(a < a)
+# print(a <= 5)
+# print(b == 2 * a)
+
+# budget = 100
+# cost = 500
+#
+# if budget >= cost:
+#     print('отиваме на ваканция')
+
+# is_budget_enough = True
+# print(is_budget_enough)
+# print(type(is_budget_enough))
+#
+# budget = 100
+# cost = 500
+#
+# is_budget_enough_2 = budget >= cost
+# print(is_budget_enough_2)
+# print(type(is_budget_enough_2))
+#
+
+
+# a = int(input())
+# is_positive = a > 0
+# print(is_positive)
+
+# user = input()
+# if user == 'Simo':
+#     print('Welcome, Simo!')
+#     print('line 2')
+#
+# print('line 3')
+
+# if condition:
+#     do_something
+#
+# always_do_something_else
+
+# color = 'red'
+# if color == 'red':
+#     print('Red')
+# else:
+#     print('Yellow')
+#     print('bye')
+
+# weather = input()
+#
+#
+# if weather == 'rain':
+#     print('take an umbrella')
+# elif weather == 'A':
+#     print('do A')
+# elif weather == 'B':
+#     print('do B')
+# elif weather == 'C':
+#     print('do C')
+# else:
+#     print('take sunglasses')
+
+# number = int(input())  # 25
+#
+# if number > 10:  # (10; +inf)
+#     print('larger than 10')
+# elif number > 20:
+#     print('larger than 20')
+# elif number > 30:
+#     print('larger than 30')
+# else:
+#     print('from else')
+
+# print('outside if-elif-else')
+
+
+# print(f'{500.28888 :.2f}')
+#
+# kg = float(input("1 кг. банани струва 5.00лв. Колко килограма желаете: "))  # 3.0
+#
+# total_price = kg * 0.005  # 3.0 * 5 = 15.0
+#
+# print(f'Струва: {total_price :.2f} лв.')
+
+# number = int(input())
+#
+# if 10 < number < 20:  # number > 10 and number < 20
+#     print('between 10 & 20')
+
+# current_day = "Monday1"
+# salary = 0
+# if current_day == "Monday":
+#     salary = 1000
+#
+# print(salary)
+
+
+# print(age)
+# age = 23
+
+# number = 12
+# if number > 100:
+#     pass
+# elif number < 20:
+#     pass
+# else:
+#     pass
+
+# number = 1000
+
+# if number > 100:
+#     print('> 100')
+# elif number > 50:
+#     print('> 50')
+
+# if number > 100:
+#     print('> 100')
+#
+# if number > 50:
+#     print('> 50')
+
+
+# figure = input()
+#
+# while figure not in ['triangle', 'circle']:
+#     print('not valid!')
+#     figure = input()
+#
+# print('some calculation after input')
+#
+# age = 13
+#
+# if age > 10:
+#     print(age)
+#     print('from inside')
+# print(age)
+
+area = 13.37
+print(f'{area:.3f}')
