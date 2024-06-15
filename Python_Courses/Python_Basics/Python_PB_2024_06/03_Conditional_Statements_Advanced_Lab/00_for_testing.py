6e80d72d0ff781094d70afbd7e267952ecd5db26
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-06-15 16:54:48 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/03_Conditional_Statements_Advanced_Lab/00_for_testing.py
@@ -0,0 +1,168 @@
+# print('it works! ✅')
+
+# THIS_IS_CONSTANT = 13
+#
+# print(THIS_IS_CONSTANT)
+# THIS_IS_CONSTANT += 1
+# print(THIS_IS_CONSTANT)
+
+# import this
+
+# is_valid = True
+# is_valid_2 = True
+#
+# if is_valid:
+#     print('do something')
+# elif is_valid_2:
+#     print('is valid 2 is True')
+# else:
+#     print('do something else')
+
+should_print = True
+
+# if should_print:
+#     print('lets print this one')
+
+# name = 'Simo'
+# print(name)
+# print(bool(name))
+#
+# if name:  # bool(name)
+#     print('name is S')
+
+# budget = 1000
+# cost = 500
+# is_budget_enough = budget >= cost  # True, False
+# print(is_budget_enough)
+# print(type(is_budget_enough))
+#
+# if is_budget_enough:
+#     print('We are going to vacation')
+
+# number = 30
+#
+# if number < 1000:
+#     print('number is less than 1000')
+#     if number > 50:
+#         print('... and number is greater than 50')
+# elif ...:
+# else:....
+
+# спестениПари = 1337
+# print(спестениПари)
+# print(type(спестениПари))
+
+# is_greater = (5 + 3) > (3 + 4)  # True, False
+# if "caseSensitive" == "CaseSensitive":
+#     print("Equal")
+# else:
+#     print("Not equal")
+# print(123456 % 10)
+# print(123456 % 100)
+# print(123456 % 1000)
+
+
+# print(123456 % 100 == 56)
+# role = "Administrator"
+# if role != "Administrator":
+#     print("No permission")
+# else:
+#     print("Welcome")
+
+# condition1 = False
+# condition2 = True
+#
+# if condition1:
+#     print('condition1 valid')
+#     if condition2:
+#         print('condition2 valid')
+#     else:
+#         print('condition2 not valid')
+# import decimal
+#
+# num1 = 0.1
+# num2 = 0.2
+#
+# print(num1 + num2)
+
+# num1 = 1.5
+# num2 = 2.5
+# num3 = 3.5
+# num4 = 4.5
+#
+# print(round(num1))
+# print(round(num2))
+# print(round(num3))
+# print(round(num4))
+
+# c = int(1)  # 1.00
+# d = float(0.7)
+# e = float(0.3)
+#
+# total = c - d - e
+# # Result = 5.551115123125783e-17
+# # това направо ме изненада :D
+# print(total)
+
+# print(2_000_000)
+# print(type(2_000_000))
+#
+# print(0.000000000000000000000000000000000000000000000000000000000000000000000023123)
+
+# num = 10.6541111
+# # print(round(num, 3))
+#
+# print(f'my num is {num :.2f}')
+
+# and
+# or
+# not
+
+# number = 76
+#
+# if 1000 > number > 50:
+#     print('number is between 50 and 1000')
+
+# is_valid = False
+#
+# if not is_valid:
+#     print('Error !!! ### 32')
+
+
+# username = input()
+# day = 'Friday'
+# print(bool(day))
+#
+# if day == 'Tuesday' or username:
+#     print('You are logged in')
+
+# num = 100
+#
+# if 10 < num < 10000 and num % 2 == 0 and not (num % 200 == 0):
+#     print('yes')
+
+# print(True or False and False)     # and has precedence
+# print((True or False) and False)   # parentheses change precedence
+#
+# print(not False or True)        # not has precedence
+# print(not (False or True))      # parentheses change precedence
+
+# number = 10_222_333
+#
+# print(number)
+# print(type(number))
+
+# help(print)
+
+# python.org
+
+# print('az', end='✅😎')
+# print('sum', end='#aaaaaaaa#')
+# print()
+# print('tuk')
+
+# print('1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
+# name = "Simo"
+# result = 155 * 13
+# # print(f'My name is: {name}')
+# print('Result is', result)
