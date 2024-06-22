e08b9153b4d9341c712e910ae102d61c94930e0b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-06-22 13:56:53 +0300

    add 06_Vowels_Sum.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/04_For_Loop_Lab/06_Vowels_Sum.py
@@ -0,0 +1,17 @@
+string = input()
+
+total_sum = 0
+
+for ch in string:
+    if ch == 'a':
+        total_sum += 1  # total_sum = total_sum + 1
+    elif ch == 'e':
+        total_sum += 2
+    elif ch == 'i':
+        total_sum += 3
+    elif ch == 'o':
+        total_sum += 4
+    elif ch == 'u':
+        total_sum += 5
+
+print(total_sum)
