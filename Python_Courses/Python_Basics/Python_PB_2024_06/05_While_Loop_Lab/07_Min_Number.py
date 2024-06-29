806ec044d498e23894760909ed719a5779ed518d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-06-29 13:47:08 +0300

    add 07_Min_Number.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/05_While_Loop_Lab/07_Min_Number.py
@@ -0,0 +1,14 @@
+# до получаване на командата "Stop", чете цели числа
+import sys
+
+min_number = sys.maxsize
+
+command = input()
+while command != "Stop":
+    current_number = int(command)
+    if current_number < min_number:
+        min_number = current_number
+
+    command = input()
+
+print(min_number)
