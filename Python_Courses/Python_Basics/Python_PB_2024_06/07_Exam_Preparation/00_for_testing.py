e3cd2c032139d864442561bdd47065fef247b74e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-07-13 12:37:09 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_06/07_Exam_Preparation/00_for_testing.py
@@ -0,0 +1,40 @@
+# # print('exam time ;)')
+#
+# num = 5
+#
+# num += 10
+# print(num)
+# num *= 10
+# print(num)
+# num /= 10
+# print(num)
+# num //= 10
+# print(num)
+# num %= 10
+# print(num)
+#
+# a = 100_00
+# print(a)
+# print(type(a))
+
+# num1 = 9876
+#
+# last_digit = num1 % 10
+# print(last_digit)
+# num1 = num1 // 10
+# last_2nd_digit = num1 % 10
+# print(last_2nd_digit)
+
+# num1 = 9876
+# num1_as_string = str(num1)
+#
+# print(num1_as_string[0])  # '9876'[0] => '9'
+# print(num1_as_string[1])
+# print(num1_as_string[2])
+# print(num1_as_string[3])
+
+num1 = '5'
+
+for i in range(int(num1), 10):
+    pass
+
