a3bd0e377cde38d3079643d1ea6b1f97d341e40a
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-10-06 13:24:21 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_10/01_First_Steps_in_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,86 @@
+# print('it works - дори и на кирилица - ✅😎')
+# print('SoftUni')
+# print('SoftUNI')
+
+# age = 20
+# Age = 22
+# aGe = 23
+
+# name = 'Simo'  # name = "Simo"
+# print(type(name))  # <class 'str'>
+#
+# age = 20.0  # age + 20
+# age_as_str = '20'
+# print(age)
+# print(type(age))  # 1. age = 20.0  2. type(20.0) == <class 'float'>  3. print(<class 'float'>)
+# print('====')
+# print(age_as_str)
+# print(type(age_as_str))
+
+# name = """
+# line 1 comment
+# line 2 comment
+# line 3 comment
+# line 4 comment
+# """
+#
+# print(name)
+
+# name = input()
+# age = int(input())  # int('13') == 13
+# weight = float(input())  # float('60') == 60.0
+#
+# print(f'Hello, {age}!!!')
+# print(type(weight))
+# print(weight)
+
+# multiline_comment = 'line1\nline2\nline3'
+# print(f'Hardcoded part... {multiline_comment}\n hardcoded part 2')
+
+# num = int(input())
+# print(num % 2)
+
+# name = input('Моля въведете името си: ')
+# print(f'Привет, {name}    😎')
+
+# print(r'\n')  # raw string
+# print('\\n')
+
+# print("Simeon's pet")
+# print('Simeon\'s pet')
+
+# user = input()
+# if user == '':
+#     exit(15)
+
+# print('zzzzzzzzzzz')
+#
+# result = 23.5678322
+#
+# print(result)
+# print(f'{result :.2f}')
+
+# print(int(7.88))  # 7 -> '7'
+
+# name = 'Shahanska'
+# age = 20
+#
+# # print(name + ' ' + str(age))
+# print(f'My name is {name}, and my next age is {age + 1}')
+
+# PENS_PRICE = 100
+#
+# print(PENS_PRICE)
+#
+# PENS_PRICE = 200
+# print(PENS_PRICE)
+
+#
+result = 23.0000000
+
+print(result)
+print(f'{result :.2f}')
+print(result)
+
+result_rounded = round(result, 3)
+print(result_rounded)
