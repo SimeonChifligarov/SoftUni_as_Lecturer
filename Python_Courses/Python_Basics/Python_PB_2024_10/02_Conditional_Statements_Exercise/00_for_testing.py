148a8bc00ce8097d6dbbd64e94558522eeec667f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-10-13 13:03:02 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_10/02_Conditional_Statements_Exercise/00_for_testing.py
@@ -0,0 +1,133 @@
+# print('it\'s working ✅')
+
+# currency = input("what is the currency? ")
+# quantity = float(input("what is the quantity? "))
+#
+# if currency == 'usd':
+#     bgn = quantity * 1.85
+#     print(f'IN BGN = {bgn}')
+# elif currency == 'eur':
+#     bgn = quantity * 1.95
+#     print(f'IN BGN = {bgn}')
+
+# num = 12
+#
+# print(type(num))  # <class 'int'>
+#
+# num_as_str = str(num)
+# print(num_as_str)
+# print(type(num_as_str))
+
+# # 1. assignment
+# num1 = 12
+#
+# # 2. cast
+# num2_as_string = '12'  # input()
+# num2 = int(num2_as_string)  # int(input())
+#
+# print(num2)
+# print(type(num2))
+#
+# # 3. run-time evaluation  -> e.g. math operators
+# num3 = 200 + 20
+# print(num3)
+# print(type(num3))
+
+# # 1. assignment
+# is_valid = True
+# # print(is_valid)
+# # print(type(is_valid))
+#
+# # 2. using bool()
+# # name = 'Simo'
+# name = ''  # '', ""
+# is_not_empty = bool(name)
+# # print(is_not_empty)  # True
+# # print(type(is_not_empty))  # <class 'bool'>
+#
+# # 3. run-time evaluation, e.g. using comparison operators
+# num = 1000
+#
+# is_greater_than_100 = num > 100  # True
+# print(is_greater_than_100)
+# print(type(is_greater_than_100))
+
+# is_valid = True  # keyword
+# true = 13.0
+
+# var = 'Simeon'
+# if var == 'Simeon':
+#     print('yes')
+# else:
+#     print('no')
+
+# num = 1
+#
+# if num > 10:
+#     print('it is greater than 10')
+#     print('line 2 inside if')
+# elif num < 0:
+#     print('num is less than 0')
+# else:
+#     print('it is not greater than 10')
+
+# num2 = 100
+#
+# print('case if-elif')
+# if num2 > 10:
+#     print('    it is greater than 10')
+# elif num2 > 20:
+#     print('    it is greater than 20')
+#
+# print('case if - if')
+# if num2 > 10:
+#     print('    it is greater than 10')
+# if num2 > 20:
+#     print('    it is greater than 20')
+
+# num = 3.141592
+#
+# print(f'{num :.2f}')
+#
+# num1 = 5
+# num2 = 314
+#
+# print(f'{num1 :03d}')
+# print(f'{num2 :03d}')
+
+# num2 = 127
+#
+# print(num2 % 2)
+# print(num2 % 2 == 0)  # if num2 is even
+# print(num2 % 10)
+# print(num2 % 100)
+# print(num2 % 100 == 7)
+
+# num3 = 10
+# print(num3)
+#
+# num3 += 15  # num3 = num3 + 15  # 25
+# print(num3)
+
+# str1 = 'Sim'
+# print(str1)
+#
+# str1 += 'eon'  # str1 = str1 + 'eon'
+# print(str1)
+
+# num = 100
+#
+# if num > 50:
+#     print('yes')
+# else:
+#     print('no')
+
+# num = 100
+# print(num)
+#
+# num -= 10  # num = num - 10
+# print(num)
+
+# print('here')
+#
+# a = 5
