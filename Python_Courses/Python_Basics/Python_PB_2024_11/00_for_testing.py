8cd146282c2c20a0461300a14ee1ba0fd776736b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-02 13:09:25 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_11/00_for_testing.py
@@ -0,0 +1,108 @@
+# # print('It is all good ✅')
+# # print('line 2')
+#
+# print('it works 1')
+# print("it works 2")
+# print('it works 1')
+# print("it works 2")
+#
+# # print('it's all good')
+# print('it\'s all good')
+# # print("it's all good")
+
+# # name = 'Симеон 😎✅'
+# name = 'Simo'  # name = "Simo"  {...}
+# age = 25  # {-100, -3, ... 0 ... 1, 2, 3...}
+# height = 1.85  # {-1.333, 3.141592, 0.0, 13.0}
+#
+# print(name)
+# print(type(name))  # <class 'str'>
+# print('---')
+# print(age)
+# print(type(age))  # <class 'int'>
+# print('---')
+# print(height)
+# print(type(height))  # <class 'float'>
+
+
+# print(type(13))
+# print(type(13.00))
+# print(13 == 13.00)
+
+# print('13')
+# print(13)
+
+# name = input()
+#
+# print(name)
+# print(type(name))
+
+
+# data = input()  # "13"
+# num = int(data)  # int('13') == 13
+#
+# # num = int(input())  # wrong => input(int())
+# print(data)
+# print(type(data))
+# print('---')
+# print(num)
+# print(type(num))
+
+# print(13 - 10)
+# print(13 – 10)
+
+# a = 5
+# b = 3
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)  # 5 // 3 == 1
+# print(a % b)  # 5 % 3 == 2  Remainder
+#
+#
+#
+# print(3 + 5 * 2)  # 13
+# print((3 + 5) * 2)  # 16
+
+# PyCharm shortcuts cheat sheet
+
+# a = 13
+# b = 22
+# print(a)
+# print(a + bb)
+
+# age = int(input("Въведи си възрастта: "))
+# name = input()
+#
+# print(name)
+
+# a = "5"
+# b = "4"
+# print(a + b)
+
+# a = "5"
+# b = 4
+# print(a + b)
+
+# name = input()
+# age = int(input())
+#
+# # print('Здрасти,' + name)
+# print(f'Здрасти, {name} ! Doubled age: {age * 2}')
+
+# name = 'Simo'
+# family_name = 'Family'
+# age = 33
+# town = 'Vidin'
+#
+# print(f'You are {name} {family_name}, a {age}-years old person from {town}.')
+# # print('You are ' + name + ' ' + family_name + ', a ' + str(age) + '-years old person from ' + town)
+
+# print(str(2.2) + str(2.2))
+# print(str(2.2 + 2.2))
+
+# import math
+#
+# math.
