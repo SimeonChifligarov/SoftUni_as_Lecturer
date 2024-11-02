87d87ed39dd5d7505b72c793404e7b927acbd07f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-02 13:11:09 +0200

    add 07_Projects_Creation.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_11/01_First_Steps_in_Coding_Lab/07_Projects_Creation.py
@@ -0,0 +1,7 @@
+# Изготвянето на един проект отнема три часа.
+architect_name = input()
+projects_count = int(input())
+
+hours_needed = projects_count * 3
+
+print(f'The architect {architect_name} will need {hours_needed} hours to complete {projects_count} project/s.')
