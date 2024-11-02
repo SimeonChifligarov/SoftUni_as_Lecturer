d08ffac5c790732b80e81b6cdef57d3e101155f1
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-02 13:10:54 +0200

    add 06_Concatenate_Data.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_11/01_First_Steps_in_Coding_Lab/06_Concatenate_Data.py
@@ -0,0 +1,9 @@
+# име, фамилия, възраст и град
+# You are <firstName> <lastName>, a <age>-years old person from <town>.
+
+name = input()
+family_name = input()
+age = int(input())
+town = input()
+
+print(f'You are {name} {family_name}, a {age}-years old person from {town}.')
