d1afe116238214448a919cb1cebcab95ce9d1c2e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-03 14:15:09 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2024_10/05_While_Loop_Exercise/00_for_testing.py
@@ -0,0 +1,31 @@
+# print('it workz ;)')
+
+# condition                    # initialization 1.
+# while condition:             # check          2.
+#    some_logic_here...
+#    change condition          # potential change 3.
+
+# number = 13  # 1. init
+#
+# while number < 100:  # 2. check
+#     print(number)
+#
+#     number += 10  # 3. change
+
+# number_or_commandEnd = int(input())
+#
+# print(number_or_commandEnd)
+# print(type(number_or_commandEnd))
+
+# for week in range(1, 3):
+#     print(f'Week - {week}:')
+#     for day in range(1, 8):
+#         if day == 4:
+#             break
+#         print(f'  --- Day - {day}:')
+
+a = 0.1
+b = 0.2
+
+print(a + b)
+print(a + b == 0.3)
