d09feca3ce96cdd0324b1b650381689bd55f8020
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-01-19 13:14:48 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_01/03_Conditional_Statements_Advanced_Exercise/00_for_testing.py
@@ -0,0 +1,132 @@
+# # print('it workz!')
+#
+# my_string = 'Simo'
+# my_empty_string = ''
+#
+# my_int = 12
+# my_zero_int = 0
+#
+# # print(bool(my_int))
+# # print(bool(my_zero_int))
+#
+# my_float = 12.0
+# my_zero_float = 0.0
+#
+# print(bool(my_float))
+# print(bool(my_zero_float))
+#
+#
+# # print(bool(my_float))
+# # print(bool(my_empty_string))
+# #
+# # if my_empty_string:
+# #     print('нещо си')
+
+# num = 100
+#
+# if num > 10:
+#     print('it is larger than 10')
+#
+# if num > 20:
+#     print('it is larger than 20')
+#
+# if num > 30:
+#     print('it is larger than 30')
+
+# num = 100
+#
+# if num > 50:
+#     print('larger than 50')
+#     if num > 750:
+#         print('larger than 75')
+
+
+# condition_1 -> value; simple check;
+# 			-> complex check;
+
+# # 1. value check
+# name = input()
+#
+# if name:
+#     print(f'hello {name}!')
+# else:
+#     print('please insert non empty string')
+
+# # 2. simple check
+# num = 100
+#
+# if num > 50:
+#     print('it is larger than 50')
+
+# # 3. complex check
+# num = 100
+#
+# if num > 50 and num % 2 == 0:
+#     print('larger than 50 and is even')
+
+# print(True or False and False)     # and has precedence
+# print((True or False) and False)   # parentheses change precedence
+#
+# print(not False or True)        # not has precedence
+# print(not (False or True))      # parentheses change precedence
+
+# my_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_var = 2000
+# print(my_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_var)
+
+# number = 100
+# #
+# if (number > 100 and number % 2 == 0 and number % 3 == 0 and number % 5 == 0
+#         and number % 12 == 0 and number % 22 == 0
+#         and number % 32 == 0 and number % 42 == 0
+#         and number % 52 == 0):
+#     print('yes')
+
+# from math import nan
+#
+# print(bool(nan))
+
+# send_mail
+#
+# if last_send_mail is before 3 days and not today is Sunday:
+#     send_mail
+
+# user_type = input()
+#
+# if not user_type == 'admin':
+#     log_as_customer
+
+# if is_admin or is_super_admin:
+#     ...
+
+# a = 5
+#
+# """
+# ...
+# """
+#
+# 'my name is Simo'
+
+# name = 'Simo'
+# if not name == 'Simo':
+#     ...
+# if name != 'Simo':
+#     ...
+
+# name = 'Todor'
+#
+# if name == 'Simo' or name == 'Georgi':
+#     print('name is Simo or Georgi')
+
+# if 'Georgi':
+#     print('here!')
+
+# discount = 0.50
+# extra_discount = 0.50
+#
+# result = 1000 * (1 - discount) * (1 - extra_discount)
+# print(result)
+#
+# result_two = 1000 * (1 - (discount + extra_discount))
+# print(result_two)
+
+# print(30 == 30.000000)
