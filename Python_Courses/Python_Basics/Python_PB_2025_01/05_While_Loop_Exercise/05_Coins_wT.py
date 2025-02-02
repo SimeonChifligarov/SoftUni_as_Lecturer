4993b18740e50b9bc7a16ae06376f9dd9ddd3316
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-02 13:17:50 +0200

    add 05_Coins_wT.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_01/05_While_Loop_Exercise/05_Coins_wT.py
@@ -0,0 +1,32 @@
+# possible coins = 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01
+
+change = float(input())
+
+total_coins = 0
+# while change > 0:
+
+while True:
+    if change <= 0:
+        break
+
+    if change >= 2:
+        change -= 2
+    elif change >= 1:
+        change -= 1
+    elif change >= 0.50:
+        change -= 0.50
+    elif change >= 0.20:
+        change -= 0.20
+    elif change >= 0.10:
+        change -= 0.10
+    elif change >= 0.05:
+        change -= 0.05
+    elif change >= 0.02:
+        change -= 0.02
+    elif change >= 0.01:
+        change -= 0.01
+
+    change = round(change, 2)
+    total_coins += 1
+
+print(total_coins)
