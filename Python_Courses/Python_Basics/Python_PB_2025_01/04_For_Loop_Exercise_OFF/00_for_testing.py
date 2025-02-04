178e140134a51642a5b319435d48eba42212cb2d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-04 16:24:46 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_01/04_For_Loop_Exercise_OFF/00_for_testing.py
@@ -0,0 +1,15 @@
+# print('hello world')
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+# else:
+#     print('finished without break')
+#
+# for i in range(10):
+#     if i == 10:
+#         break
+#     print(i)
+# else:
+#     print('finished without break')
