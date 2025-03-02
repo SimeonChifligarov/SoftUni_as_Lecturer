01338c89d644e01282277ee7b54e0fb21f0ed8d9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-02 13:21:35 +0200

    add 02_Radians_to_Degrees.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_03/01_First_Steps_in_Coding_Exercise/02_Radians_to_Degrees.py
@@ -0,0 +1,13 @@
+# градус = радиан * 180 / π.
+# Числото π в Python може да достъпите чрез модула math.
+
+# 1. E
+from math import pi
+
+radians = float(input())  # float('3.141592')
+
+# 2. T
+degrees = radians * 180 / pi
+
+# 3. L
+print(degrees)
