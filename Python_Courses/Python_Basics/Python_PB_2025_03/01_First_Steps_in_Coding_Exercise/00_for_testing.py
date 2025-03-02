900c99027feb13ac8292fa6b6a50c6d53aed2b3a
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-02 13:21:00 +0200

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_03/01_First_Steps_in_Coding_Exercise/00_for_testing.py
@@ -0,0 +1,80 @@
+# # first_name = 'Simo'
+# # last_name = 'Chifligarov'
+#
+# name1 = 'Simo'
+# name2 = 'Simo'
+#
+# print(name1)
+# print(name2)
+# # print(name1 == name2)
+#
+# age = 23
+# Age = 32
+#
+# print(age)
+
+# name1 = 'Simo'
+# name2 = "Simo"
+#
+# print(name1)
+# print(type(name1))  # <class 'str'>
+#
+# print(name2)
+# print(type(name2))
+# print(name1 == name2)
+
+# age = 23
+#
+# print(age)
+# print(type(age))  # <class 'int'>
+#
+# weight = 56.6
+#
+# print(weight)
+# print(type(weight))  # <class 'float'>
+
+# age = '23'
+# print(age + 1)
+
+# price = 34.0
+# print(price)
+# print(type(price))  # float
+
+# print(1300 + 37)  # here we sum 1300 + 37 and then print the result
+
+# age = int(30)  # int(int...))
+# age = 30
+
+# print(type(30))
+# name = 'Simo'
+#
+# print(f'Hello, {name}!!!')  # f-string
+#
+# name = 'SimeonC'
+# print(f'Hello, {name}!!!')
+#
+# age = 23
+# print(f'next age is {age + 1}')
+
+# name = input()  #
+# age = int(input())  # int('22') == 22
+#
+# # age_as_int = int(age)  # int('22') == 22
+#
+# print(age)
+# print(type(age))
+#
+# print(f'Hello -> {name}, next age is {age + 1} ###')
+# # print(33)  # str(33) == '33'
+#
+# print(3 + 2 * 10)  # 23
+# print((3 + 2) * 10)  # 50
+#
+# print(float('13'))  # 13.0
+# print(float('13.5'))  # 13.5
+#
+# print(int('22'))  # 22
+# print(int('22.2'))  # Error
+
+# num = 13.33777
+# print(f'{num:.2f} euro')
