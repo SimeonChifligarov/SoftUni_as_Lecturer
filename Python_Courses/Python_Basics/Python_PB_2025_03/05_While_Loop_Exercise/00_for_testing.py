6d11fa5490821b97549f69aa61a2ea96ea725b5d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-30 13:09:29 +0300

    add 00_for_testing.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_03/05_While_Loop_Exercise/00_for_testing.py
@@ -0,0 +1,71 @@
+# print('it is all good ;)')
+
+# for el in collection:
+#     ...
+#
+# [pseudocode]
+#
+# for st in students_mar_2025:
+#     send_email(st.exam_result)
+#
+#     if st.exam_result >= 5.00:
+#         print_certificate(st)
+
+# [pseudocode]
+#
+# while not email.open:
+#     resend(email, period=days_3)
+
+# import time
+
+# number = 13  # 1. init
+# while number < 100:  # 2. check
+#     # time.sleep(1)
+#
+#     if number == 43 or number == 53:
+#         print('I am here')
+#         number += 10  # 3. potential change
+#         continue
+#
+#     print(number)
+#
+#     number += 10  # 3. potential change
+#
+#
+# print('outside while loop')
+# print(f'number value: {number}')
+
+# number = 13
+#
+# while True:
+#     print(number)
+#     number += 10
+#
+#     if number >= 100:
+#         break
+#
+# print('outside while loop')
+# print(f'number value: {number}')
+
+# for
+# while
+
+# range
+
+# num1 = 0.1
+# num2 = 0.2
+#
+# num3 = num1 + num2
+#
+# print(num3)
+# print(num3 == 0.3)
+
+# print('Num1', 'Num2', 'print3', sep='###')
+
+# num1 = 1
+# num2 = 2
+# num3 = num1 + num2
+#
+# print(num3)
+
+# print(1.23 * 100)
