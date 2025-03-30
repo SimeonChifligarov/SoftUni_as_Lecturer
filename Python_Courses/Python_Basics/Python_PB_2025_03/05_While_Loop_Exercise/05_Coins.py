61ed5d3a7f1ef4b48927ec652744dfaedae9cbaf
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-30 13:10:56 +0300

    add 05_Coins.py

---
+++ Python_Courses/Python_Basics/Python_PB_2025_03/05_While_Loop_Exercise/05_Coins.py
@@ -0,0 +1,28 @@
+coins = 0
+
+change = float(input())  # e.g. float('1.23') == 1.23
+
+while change > 0:
+
+    if change >= 2:
+        change -= 2
+    elif change >= 1:
+        change -= 1
+    elif change >= 0.50:
+        change -= 0.50
+    elif change >= 0.20:
+        change -= 0.20
+    elif change >= 0.10:
+        change -= 0.10
+    elif change >= 0.05:
+        change -= 0.05
+    elif change >= 0.02:
+        change -= 0.02
+    elif change >= 0.01:
+        change -= 0.01
+
+    change = round(change, 2)
+
+    coins += 1
+
+print(coins)
